b085f74a94e3233b78378025c4cb7254b53057f8
Nicola Graziotin <nicola.graziotin@studio.unibo.it>
2024-01-22 18:36:16 +0100

    added key listener to the frame

--- src/main/java/it/unibo/view/GameWindow.java
+++ src/main/java/it/unibo/view/GameWindow.java
@@ -4,6 +4,8 @@ import java.awt.Color;
 
 import javax.swing.JFrame;
 
+import it.unibo.commons.input.KeyboardInput;
+
 public class GameWindow extends JFrame {
 
     public GameWindow() {
@@ -12,5 +14,7 @@ public class GameWindow extends JFrame {
         panel.setSize(this.getSize());
         panel.setBackground(Color.CYAN);
         this.setContentPane(panel);
+
+        addKeyListener(new KeyboardInput(panel));
     }
 }
