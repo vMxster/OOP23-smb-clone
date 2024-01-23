c042e68bdce37badd957f47a2c02a0f1af06d8a2
Mattia <mattia.galli9@studio.unibo.it>
2024-01-23 15:35:23 +0100

    changed class CircularSawImpl.java to extend AbstractEntity

--- src/main/java/it/unibo/model/entity/obstacles/CircularSawImpl.java
+++ src/main/java/it/unibo/model/entity/obstacles/CircularSawImpl.java
@@ -3,46 +3,15 @@ package it.unibo.model.entity.obstacles;
 import java.awt.Color;
 import java.awt.Graphics2D;
 
-public class CircularSawImpl implements CircularSaw{
+import it.unibo.model.entity.AbstractEntityImpl;
 
-    private double x;
-    private double y;
-    private double width;
-    private double height;
+public class CircularSawImpl extends AbstractEntityImpl implements CircularSaw{
+
     private double radius;
 
     public CircularSawImpl(final double x, final double y, final double width, final double height, final double radius) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
+        super(x, y, width, height);
         this.radius = radius;
-    }
-
-    @Override
-    public double getX() {
-        return this.x; 
-    }
-
-    @Override
-    public double getY() {
-        return this.y;
-    }
-
-    @Override
-    public double getWidth() {
-        return this.width;
-    }
-
-    @Override
-    public double getHeight() {
-        return this.height;
-    }
-
-    @Override
-    public void update() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'update'");
     }
 
     @Override
