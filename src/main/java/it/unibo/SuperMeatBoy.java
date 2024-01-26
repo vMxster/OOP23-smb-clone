e38e1cb77600bc494e52b63eef339dbdc9033310
Martin Tomassi <martin.tomassi@studio.unibo.it>
2024-01-26 22:22:58 +0100

    Modified the Main Class - Added the Controller Parameter to the GameWindow Constructor

--- src/main/java/it/unibo/SuperMeatBoy.java
+++ src/main/java/it/unibo/SuperMeatBoy.java
@@ -2,14 +2,18 @@ package it.unibo;
 
 import it.unibo.view.GameWindow;
 import it.unibo.commons.Constants;
+import it.unibo.controller.ControllerImpl;
 
+import java.io.IOException;
+import java.net.URL;
 
 import javax.swing.JFrame;
 
 public class SuperMeatBoy {
 
-    public static void main(String[] args) {
-        GameWindow frame = new GameWindow();
+    public static void main(String[] args) throws IOException {
+        URL urlMap = new URL("resources/factory1.tmx");
+        GameWindow frame = new GameWindow(new ControllerImpl(urlMap));
 
         frame.setSize(Constants.SW / Constants.PROPORTION, Constants.SH / Constants.PROPORTION);
         frame.setLocationByPlatform(true);
