4c3b828ccd2c282162ba63fad937be25b47eb2af
Martin Tomassi <martin.tomassi@studio.unibo.it>
2024-01-27 12:48:57 +0100

    Modified Constants Class - Added String Source of the TMX File

--- src/main/java/it/unibo/commons/Constants.java
+++ src/main/java/it/unibo/commons/Constants.java
@@ -8,11 +8,11 @@ public class Constants {
     public static final int MEATBOY_WIDTH = 20;
 	public static final int MEATBOY_HEIGHT = 20;
     public static final int TILE_SIZE = 20;
-
     public static final int PROPORTION = 2;
 
-    private final static Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+    public static final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
     public static final int SW = (int) screen.getWidth();
     public static final int SH = (int) screen.getHeight();
+    public static final String STRING_MAP = "file:./src/main/resources/factory1.tmx";
 
 }
