d7f62340638f88548e0b662b78ef8cc424d41a3a
Martin Tomassi <martin.tomassi@studio.unibo.it>
2024-01-27 22:34:47 +0100

    Modified TileSet Implementation of TileSet Interface - Added Parameter to the Tile Constructor that instances it

--- src/main/java/it/unibo/model/tiles/TileSetImpl.java
+++ src/main/java/it/unibo/model/tiles/TileSetImpl.java
@@ -53,7 +53,7 @@ public class TileSetImpl implements TileSet {
 	public void divideSpriteSheet(int width, int height, String srcImage) throws IOException {
 		for( int row=0 ; row<height ; row+=20 ) {
 			for( int column=0 ; column<width ; column+=20 ) {	
-				tiles.add(new TileImpl(row, column));
+				tiles.add(new TileImpl(row, column, srcImage));
 			}
 		}
 	}
