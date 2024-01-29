17d21ce7718f8cc72a918114f9a8bb05d7835700
Martin Tomassi <martin.tomassi@studio.unibo.it>
2024-01-29 21:00:08 +0100

    Modified TileLoader Interface - Removed loadTiles Method

--- src/main/java/it/unibo/model/tiles/TileLoader.java
+++ src/main/java/it/unibo/model/tiles/TileLoader.java
@@ -1,22 +1,11 @@
 package it.unibo.model.tiles;
 
-import java.util.List;
-
 /**
  * The TileLoader interface provides methods for loading various types of tiles, 
  * stationary elements, platforms, and circular saws from an XML document 
  * into corresponding lists or instances.
  */
 public interface TileLoader {
-
-    /**
-     * Loads tiles from the specified layer in the XML document into the given tileList.
-     * Parses the specified layer in the document and populates the tileList with the extracted tiles.
-     *
-     * @param tileList The list to populate with tiles. It is a List of Lists of Tile objects.
-     * @param layerNameToParse The name of the layer in the XML document to be parsed.
-     */
-    void loadTiles(final List<List<Tile>> tileList, final String layerNameToParse);
 
     /**
  	 * Loads stationary tiles from the XML document into the stationary list.
