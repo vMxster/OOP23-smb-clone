fa616d0ae2563155da6c8f114a8762f86173ea8e
Martin Tomassi <martin.tomassi@studio.unibo.it>
2024-01-29 15:31:16 +0100

    Modified Tile Implementation of Tile Interface - Removed Identifier Getter and Field ; Removed generateImageIdentifier Method

--- src/main/java/it/unibo/model/tiles/TileImpl.java
+++ src/main/java/it/unibo/model/tiles/TileImpl.java
@@ -4,7 +4,6 @@ public class TileImpl implements Tile {
 
     private final int x;
     private final int y;
-    private final String identifier;
     private final String srcImage;
     
     /**
@@ -17,7 +16,6 @@ public class TileImpl implements Tile {
         this.x = x;
         this.y = y;
         this.srcImage = srcImage;
-        this.identifier = generateImageIdentifier();
     }
 
     @Override
@@ -33,21 +31,6 @@ public class TileImpl implements Tile {
     @Override
     public String getSrcImage() {
         return this.srcImage;
-    }
-
-    @Override
-    public String getIdentifier() {
-        return this.identifier;
-    }
-
-    /**
-     * Generates a unique identifier for the image associated with the Tile.
-     * The identifier is constructed using the X and Y coordinates of the Tile.
-     *
-     * @return A string representing the image identifier.
-     */
-    private String generateImageIdentifier() {
-        return "Image_" + this.x + "_" + this.y;
     }
 
 }
