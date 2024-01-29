952d99678a1f44a60985ccd1315d7256d98b3831
Martin Tomassi <martin.tomassi@studio.unibo.it>
2024-01-29 19:29:25 +0100

    Modified Constants Class

--- src/main/java/it/unibo/commons/Constants.java
+++ src/main/java/it/unibo/commons/Constants.java
@@ -1,18 +1,13 @@
 package it.unibo.commons;
 
-import java.awt.Dimension;
-import java.awt.Toolkit;
-
 public class Constants {
     
     public static final int MEATBOY_WIDTH = 20;
 	public static final int MEATBOY_HEIGHT = 20;
     public static final int TILE_SIZE = 20;
     public static final int PROPORTION = 2;
-
-    public static final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
-    public static final int SW = (int) screen.getWidth();
-    public static final int SH = (int) screen.getHeight();
-    public static final String STRING_MAP = "file:./src/main/resources/factory1.tmx";
+    public static final int SW = 800;
+    public static final int SH = 600;
+    public static final String SOURCE_MAP = "file:./src/main/resources/factory1.tmx";
 
 }
