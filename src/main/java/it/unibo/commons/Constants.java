bc9088573b29e1bf32d8094c3b798523e621b268
Martin Tomassi <martin.tomassi@studio.unibo.it>
2024-01-30 16:52:14 +0100

    Modified Constants Class - Added ID_TILE_BANDAGEGIRL, ID_TILE_MEATBOY and ID_TILE_NULL used for parsing the TMX

--- src/main/java/it/unibo/commons/Constants.java
+++ src/main/java/it/unibo/commons/Constants.java
@@ -2,12 +2,12 @@ package it.unibo.commons;
 
 public class Constants {
     
-    public static final int MEATBOY_WIDTH = 20;
-	public static final int MEATBOY_HEIGHT = 20;
     public static final int TILE_SIZE = 20;
-    public static final int PROPORTION = 2;
     public static final int SW = 800;
-    public static final int SH = 600;
+    public static final int SH = 630;
+    public static final int ID_TILE_BANDAGEGIRL = 902;
+    public static final int ID_TILE_MEATBOY = 901;
+    public static final int ID_TILE_NULL = 0;
     public static final String SOURCE_MAP = "file:./src/main/resources/factory1.tmx";
 
 }
