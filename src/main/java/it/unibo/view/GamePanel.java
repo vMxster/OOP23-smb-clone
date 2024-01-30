a454d138df3318ad827df600a2302d581deb6712
Martin Tomassi <martin.tomassi@studio.unibo.it>
2024-01-30 17:16:02 +0100

    Modified GamePanel Class - Modified setImages Implementation to collect the Images in a List and then, using paintComponent, with a for loop, draw them (BETA IMPLEMENTATION)

--- src/main/java/it/unibo/view/GamePanel.java
+++ src/main/java/it/unibo/view/GamePanel.java
@@ -7,36 +7,27 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import javax.swing.JPanel;
 import java.awt.image.BufferedImage; 
 
 import it.unibo.commons.Constants;
-import it.unibo.model.entity.obstacles.CircularSaw;
-import it.unibo.model.entity.obstacles.CircularSawImpl;
-import it.unibo.model.entity.obstacles.Platform;
-import it.unibo.model.entity.obstacles.PlatformImpl;
 import it.unibo.model.entity.player.MeatBoy;
 import it.unibo.model.entity.player.MeatBoyImpl;
-import it.unibo.model.entity.target.BandageGirl;
-import it.unibo.model.entity.target.BandageGirlImpl;
 
 public class GamePanel extends JPanel implements ActionListener {
 
     private final MeatBoy meatBoy;
-    private final BandageGirl bandageGirl;
-    private final CircularSaw saw;
-    private final Platform platform;
     private final Timer gameTimer;
-    private BufferedImage backgroundImage;
+    private final List<BufferedImage> images;
 
     public GamePanel() {
         
-        this.meatBoy = new MeatBoyImpl(200, 200, Constants.MEATBOY_WIDTH, Constants.MEATBOY_HEIGHT);
-        this.bandageGirl = new BandageGirlImpl(250, 200, Constants.MEATBOY_WIDTH, Constants.MEATBOY_HEIGHT);
+        this.meatBoy = new MeatBoyImpl(200, 200, Constants.TILE_SIZE, Constants.TILE_SIZE);
         this.gameTimer = new Timer();
-        this.saw = new CircularSawImpl(100, 150, 50, 50, 50);
-        this.platform = new PlatformImpl(150, 150, 80, 20);
+        this.images = new ArrayList<>();
         this.gameTimer.schedule(new TimerTask() {
 
             @Override
@@ -48,8 +39,8 @@ public class GamePanel extends JPanel implements ActionListener {
         }, 0, 17);
     }
 
-    public void setBackgroundImage(BufferedImage backgroundImage) {
-        this.backgroundImage = backgroundImage;
+    public void setImages(final List<BufferedImage> images) {
+        this.images.addAll(images);
         repaint();
     }
 
@@ -57,11 +48,10 @@ public class GamePanel extends JPanel implements ActionListener {
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
-        g2d.drawImage(Objects.requireNonNull(backgroundImage), 0, 0, this);
+        for (int i=0 ; i<this.images.size() ; i++) {
+            g2d.drawImage(Objects.requireNonNull(this.images.get(i)), 0, 0, this);
+        }
         this.meatBoy.draw(g2d);
-        this.saw.draw(g2d);
-        this.platform.draw(g2d);
-        this.bandageGirl.draw(g2d);
     }
 
     @Override
