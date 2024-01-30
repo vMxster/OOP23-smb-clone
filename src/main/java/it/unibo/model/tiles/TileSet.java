959e76ba2e83a6bb317aef94025c97bcfea35029
Martin Tomassi <martin.tomassi@studio.unibo.it>
2024-01-30 12:33:16 +0100

    Modified TileSet Interface - Removed divideSpriteSheet Method, changing visibility from public to private

--- src/main/java/it/unibo/model/tiles/TileSet.java
+++ src/main/java/it/unibo/model/tiles/TileSet.java
@@ -10,14 +10,6 @@ import java.util.List;
  * Implementing classes should provide methods to access and manipulate the TileSet.
  */
 public interface TileSet {
-    
-    /**
-	 * Splits the SpriteSheet into tiles of the appropriate size.
-	 * @param w The width of the TileSet image
-	 * @param h The height of the TileSet image
-	 * @throws IOException
-	 */
-    void divideSpriteSheet(int width, int height, String src) throws IOException;
 
     /**
 	 * Analyzes the tmx file to identify the appropriate SpriteSheets.
