f3ade58dc63e25ea082c6b661a14b6907a7588ae
Martin Tomassi <martin.tomassi@studio.unibo.it>
2024-01-30 18:04:38 +0100

    Modified Level Implementation of Level Interface - Removed Platforms,Saws and Model Field and Getters use TileManager to return the object requested

--- src/main/java/it/unibo/model/level/LevelImpl.java
+++ src/main/java/it/unibo/model/level/LevelImpl.java
@@ -3,68 +3,56 @@ package it.unibo.model.level;
 import java.net.URL;
 import java.util.List;
 
-import it.unibo.commons.Constants;
-import it.unibo.model.entity.player.MeatBoyImpl;
-import it.unibo.model.entity.target.BandageGirl;
+import it.unibo.model.tiles.Tile;
 import it.unibo.model.tiles.TileManager;
 import it.unibo.model.tiles.TileManagerImpl;
-import it.unibo.model.GameModel;
 import it.unibo.model.entity.obstacles.CircularSaw;
 import it.unibo.model.entity.obstacles.Platform;
-import it.unibo.model.entity.player.MeatBoy;
+//import it.unibo.model.entity.player.MeatBoy;
+//import it.unibo.model.entity.target.BandageGirl;
 
 public class LevelImpl implements Level{
 
-	private final GameModel model;
     private final TileManager tileManager;
-    private final List<Platform> platforms;
-	private final List<CircularSaw> circularSaws;
-    private final BandageGirl bandageGirl;
-	private final MeatBoy meatBoy;
-	private final int widthLevel;
-	private final int heightLevel;
+	//private final MeatBoy meatBoy;
+	//private final BandageGirl bandageGirl;
 
-	public LevelImpl(final GameModel model, final URL urlMap) {
- 		this.model = model;
+	public LevelImpl(final URL urlMap) {
 		this.tileManager = new TileManagerImpl(urlMap);
-		this.platforms = tileManager.getPlatforms();
-		this.circularSaws = tileManager.getSaws();
-		this.meatBoy = new MeatBoyImpl(tileManager.getPlayerCoordStart().getX(), tileManager.getPlayerCoordStart().getY(), Constants.TILE_SIZE, Constants.TILE_SIZE);
-		this.bandageGirl = tileManager.getBandageGirl();
-		this.widthLevel = tileManager.getNumCols() * Constants.TILE_SIZE;
-		this.heightLevel = tileManager.getNumRows() * Constants.TILE_SIZE;
+		//this.meatBoy = tileManager.getMeatBoy();
+		//this.bandageGirl = tileManager.getBandageGirl();	
 	}
 	
 	@Override
 	public void update() {
-		//if(meatBoy.getHitbox().intersects(bandageGirl.getHitbox())) {
-		//	model.finished();
+		//if (meatBoy.getHitbox().win(bandageGirl)) {
+		//	model.finish();
 		//}
 	}
 
 	@Override
+	public List<List<Tile>> getStationary() {
+		return this.tileManager.getStationary();
+	}
+
+	@Override
 	public List<Platform> getPlatforms() {
-		return this.platforms;
+		return this.tileManager.getPlatforms();
 	}
 
 	@Override
-	public List<CircularSaw> getCircularSaws() {
-		return this.circularSaws;
+	public List<CircularSaw> getSaws() {
+		return this.tileManager.getSaws();
 	}
 
 	@Override
-	public int getWidthLevel() {
-		return this.widthLevel;
+	public int getNumCols() {
+		return this.tileManager.getNumCols();
 	}
 
 	@Override
-	public int getHeightLevel() {
-		return this.heightLevel;
-	}
-
-	@Override
-	public TileManager getTileManager() {
-		return this.tileManager;
+	public int getNumRows() {
+		return this.tileManager.getNumRows();
 	}
 
 }
