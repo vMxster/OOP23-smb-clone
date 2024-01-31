1233ecafe181782c38f1c2810b6a753bc5bcc470
Marcatelli Francesco <francesco.marcatelli@studio.unibo.it>
2024-01-31 13:11:08 +0100

    Modified CircularHitbox field and modified TileLoader method - Removed width and height from CirculaHitbox and changed SetX() into SetY() in tileLoader()

--- src/main/java/it/unibo/model/hitbox/CircularHitbox.java
+++ src/main/java/it/unibo/model/hitbox/CircularHitbox.java
@@ -7,16 +7,14 @@ import java.awt.geom.Ellipse2D;
 public class CircularHitbox implements Hitbox<Ellipse2D> {
 
     private final Ellipse2D hitbox;
-    private final double radius;
     
     public CircularHitbox(final double x, final double y, final double radius) {
         this.hitbox = new Ellipse2D.Double(x, y, radius, radius);
-        this.radius = radius;
     }
 
     @Override
     public void updatePosition(double x, double y) {
-        this.hitbox.setFrame(x, y, radius, radius);
+        this.hitbox.setFrame(x, y, hitbox.getWidth(), hitbox.getHeight());
     }
 
     @Override
