23387c65bda89a01aea0f4266513a42450c2ae3c
Nicola Graziotin <nicola.graziotin@studio.unibo.it>
2024-02-03 13:00:20 +0100

    Modified Main - modified game controller implementation

--- src/main/java/it/unibo/SuperMeatBoy.java
+++ src/main/java/it/unibo/SuperMeatBoy.java
@@ -1,24 +1,10 @@
 package it.unibo;
 
-import it.unibo.view.GameWindow;
-import it.unibo.commons.Constants;
 import it.unibo.controller.GameControllerImpl;
-
-import java.io.IOException;
-import java.net.URL;
-
-import javax.swing.JFrame;
 
 public class SuperMeatBoy {
 
-    public static void main(String[] args) throws IOException {
-        GameWindow frame = new GameWindow(new GameControllerImpl(new URL(Constants.SOURCE_MAP)));
-
-        frame.setSize(Constants.SW, Constants.SH);
-        frame.setResizable(false);
-        frame.setLocationByPlatform(true);
-        frame.setTitle("Super Meat Boy Clone");
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    public static void main(String[] args) {
+        new GameControllerImpl();
     }
 }
