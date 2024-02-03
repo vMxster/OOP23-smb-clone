e2a502870d3195bbdf556911ff8c919fe8ea4393
Nicola Graziotin <nicola.graziotin@studio.unibo.it>
2024-02-03 13:01:44 +0100

    Modified GameControllerImpl - moved game loop in game controller class

--- src/main/java/it/unibo/controller/GameControllerImpl.java
+++ src/main/java/it/unibo/controller/GameControllerImpl.java
@@ -1,27 +1,58 @@
 package it.unibo.controller;
 
-import java.net.URL;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
+import it.unibo.commons.Constants;
 import it.unibo.model.GameModel;
 import it.unibo.model.GameModelImpl;
+import it.unibo.model.collision.CollisionChecker;
 import it.unibo.model.entity.obstacles.CircularSaw;
 import it.unibo.model.entity.obstacles.Platform;
 import it.unibo.model.entity.player.MeatBoy;
 import it.unibo.model.entity.target.BandageGirl;
 import it.unibo.model.tiles.Tile;
 
+import it.unibo.view.GameWindow;
+
 public class GameControllerImpl implements GameController {
 
     private final GameModel gameModel;
+    private final GameWindow gameWindow;
+    private final Timer gameTimer;
 
-    //private final GameView gameView;
-    private final URL urlMap;
+    public GameControllerImpl() {
+        this.gameModel = new GameModelImpl(Constants.SOURCE_MAP);
+        this.gameWindow = new GameWindow(this);
+        this.gameTimer = new Timer();
+        this.start();
+    }
 
-    public GameControllerImpl(final URL urlMap) {
-        this.gameModel = new GameModelImpl(urlMap);
-        //this.gameView = new GameView(this);
-        this.urlMap = urlMap;
+    private void start() {
+        this.gameTimer.schedule(new TimerTask() {
+
+            @Override
+            public void run() {
+                gameModel.getCollisionChecker().updateMeatBoy();
+                gameWindow.repaint();
+                if (gameModel.getCollisionChecker().isInWindow() == CollisionChecker.CollisionState.FALL) {
+                    try {
+                        Thread.sleep(500);
+                        System.exit(0);
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                } else {
+                    if (gameModel.getCollisionChecker().isColliding() == (CollisionChecker.CollisionState.SAW)) {
+                        System.out.println("HAI PERSO");      
+                    } else if (gameModel.getCollisionChecker().isColliding() == (CollisionChecker.CollisionState.BANDAGE_GIRL)) {
+                        System.out.println("HAI VINTO");
+                    }  
+                }   
+            }
+            
+        }, 0, 17);
     }
 
     @Override
@@ -37,11 +68,6 @@ public class GameControllerImpl implements GameController {
     @Override
     public List<Platform> getPlatforms() {
         return this.gameModel.getPlatforms();
-    }
-
-    @Override
-    public String getTmxURL() {
-        return this.urlMap.getPath();
     }
 
     @Override
--- src/main/java/it/unibo/view/GamePanel.java
+++ src/main/java/it/unibo/view/GamePanel.java
@@ -1,60 +1,28 @@
 package it.unibo.view;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
-import java.util.Timer;
-import java.util.TimerTask;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import javax.swing.JPanel;
-import java.awt.image.BufferedImage; 
+import java.awt.image.BufferedImage;
 
+import it.unibo.commons.Constants;
 import it.unibo.controller.GameController;
-import it.unibo.model.collision.CollisionChecker;
-import it.unibo.model.collision.CollisionCheckerImpl;
 import it.unibo.model.entity.player.MeatBoy;
 
 public class GamePanel extends JPanel{
-
-    private final MeatBoy meatBoy;
-    private final Timer gameTimer;
     private final List<BufferedImage> images;
     private GameController controller;
-    private CollisionChecker collisionChecker;
+    private final MeatBoy meatBoy;
 
     public GamePanel(GameController controller) {
-        
+        this.images = new ArrayList<>();
         this.controller = controller;
         this.meatBoy = this.controller.getMeatBoy();
-        this.collisionChecker = new CollisionCheckerImpl(controller.getGameModel());
-        this.gameTimer = new Timer();
-        this.images = new ArrayList<>();
-        this.gameTimer.schedule(new TimerTask() {
-
-            @Override
-            public void run() {
-                collisionChecker.updateMeatBoy();
-                repaint();
-                if (collisionChecker.isInWindow() == CollisionChecker.CollisionState.FALL) {
-                    try {
-                        Thread.sleep(500);
-                        System.exit(0);
-                    } catch (InterruptedException e) {
-                        Thread.currentThread().interrupt();
-                    }
-                } else {
-                    if (collisionChecker.isColliding() == (CollisionChecker.CollisionState.SAW)) {
-                        System.out.println("HAI PERSO");      
-                    } else if (collisionChecker.isColliding() == (CollisionChecker.CollisionState.BANDAGE_GIRL)) {
-                        System.out.println("HAI VINTO");
-                    }  
-                    
-                }   
-            }
-            
-        }, 0, 17);
     }
 
     public void setImages(final List<BufferedImage> images) {
@@ -69,15 +37,20 @@ public class GamePanel extends JPanel{
         for (int i=0 ; i<this.images.size() ; i++) {
             g2d.drawImage(Objects.requireNonNull(this.images.get(i)), 0, 0, this);
         }
-        this.meatBoy.draw(g2d);
+        paintMeatBoy(g2d);
+    }
+
+    void paintMeatBoy(Graphics2D g2d) {
+        g2d.setColor(Color.BLACK);
+        g2d.fillRect((int)meatBoy.getX(), (int)meatBoy.getY(), Constants.TILE_SIZE, Constants.TILE_SIZE);
     }
 
     public void keyPressed(KeyEvent e) {
-        this.collisionChecker.moveMeatBoy(e.getKeyCode());
+        this.controller.getGameModel().getCollisionChecker().moveMeatBoy(e.getKeyCode());
     }
 
     public void keyReleased(KeyEvent e) {
-        this.collisionChecker.stopMovingMeatBoy(e.getKeyCode());
+        this.controller.getGameModel().getCollisionChecker().stopMovingMeatBoy(e.getKeyCode());
     }
 
 }
