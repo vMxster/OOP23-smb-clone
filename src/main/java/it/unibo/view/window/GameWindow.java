4047ac16f5e15bb7dc902fc37477d27faf3c3155
Martin Tomassi <martin.tomassi@studio.unibo.it>
2024-02-04 20:44:52 +0100

    Added GameWindow Interface - GameWindow represents an interface now, no longer an implementation of GameWindow

--- src/main/java/it/unibo/view/window/GameWindow.java
+++ src/main/java/it/unibo/view/window/GameWindow.java
@@ -1,46 +1,23 @@
 package it.unibo.view.window;
 
-import java.util.List;
+/**
+ * The GameWindow interface represents a window for a game application.
+ * It defines methods for initializing window properties and the game panel.
+ */
+public interface GameWindow {
 
-import javax.swing.JFrame;
+    /**
+     * Initializes the properties of the game window, such as size, title, and behavior.
+     * This method should be called to set up the initial configuration of the game window.
+     */
+    void initializeWindowProperties();
 
-import it.unibo.commons.Constants;
-import it.unibo.commons.input.KeyboardInput;
-import it.unibo.controller.GameController;
-import it.unibo.view.imageRenderer.ImageRenderer;
-import it.unibo.view.imageRenderer.ImageRendererImpl;
-import it.unibo.view.panel.GamePanel;
-
-public class GameWindow extends JFrame {
-
-    private final GameController controller;
-    private final ImageRenderer renderer;
-
-    public GameWindow(final GameController controller) {
-
-        this.setSize(Constants.SW, Constants.SH);
-        this.setResizable(false);
-        this.setLocationByPlatform(true);
-        this.setTitle("Super Meat Boy Clone");
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-        this.controller = controller;
-        this.renderer = new ImageRendererImpl(this.controller.getNumRows(), this.controller.getNumCols());
-        
-        GamePanel panel = new GamePanel(controller);
-        panel.setLocation(0,0);
-        try {
-            panel.setImages(
-            List.of(
-                renderer.getBackGround(),
-                renderer.getStationary(controller.getStationary()),
-                renderer.getSaws(controller.getSaws())));
-                this.setContentPane(panel);
-                addKeyListener(new KeyboardInput(panel));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        
-        this.setVisible(true);
-    }
+    /**
+     * Initializes the game panel, setting up its layout and content.
+     * This method is responsible for creating and configuring the game panel
+     * that will be displayed within the game window.
+     */
+    void initializeGamePanel();
+    
 }
+
