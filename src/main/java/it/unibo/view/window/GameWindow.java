de8d02f067471cbe599c5b9e96e3b24d1a0a917e
Martin Tomassi <martin.tomassi@studio.unibo.it>
2024-02-04 23:16:17 +0100

    Modified GameWindow Interface - Added Paint and DisplayVictoryMessage Method

--- src/main/java/it/unibo/view/window/GameWindow.java
+++ src/main/java/it/unibo/view/window/GameWindow.java
@@ -7,6 +7,13 @@ package it.unibo.view.window;
 public interface GameWindow {
 
     /**
+     * Paints the content of the game window.
+     * This method is responsible for updating and rendering the content of the game window.
+     * It should be called whenever the content needs to be refreshed or repainted.
+     */
+    void paint();
+
+    /**
      * Initializes the properties of the game window, such as size, title, and behavior.
      * This method should be called to set up the initial configuration of the game window.
      */
@@ -18,6 +25,12 @@ public interface GameWindow {
      * that will be displayed within the game window.
      */
     void initializeGamePanel();
+
+    /**
+     * This method is responsible for showing a graphical message or UI elements
+     * indicating the player's victory.
+     */
+    void displayVictoryMessage();
     
 }
 
