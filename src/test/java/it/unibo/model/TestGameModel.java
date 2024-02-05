eaebc570d5320730ce0679c25869fc8955209f4d
Martin Tomassi <martin.tomassi@studio.unibo.it>
2024-02-05 21:41:51 +0100

    Modified Test of GameModel

--- src/test/java/it/unibo/model/TestGameModel.java
+++ src/test/java/it/unibo/model/TestGameModel.java
@@ -1,27 +1,27 @@
 package it.unibo.model;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.List;
 
+import it.unibo.commons.Constants;
+import it.unibo.model.entity.obstacles.CircularSaw;
+import it.unibo.model.entity.obstacles.Platform;
+import it.unibo.model.entity.player.MeatBoy;
+import it.unibo.model.entity.target.BandageGirl;
 import it.unibo.model.tiles.Tile;
+import it.unibo.commons.Point2D;
 
 public class TestGameModel {
 
-    private URL urlMap;
     private GameModel gameModel;
 
     @BeforeEach
-    public void init() throws MalformedURLException {
-        this.urlMap = new URL("file:./src/main/resources/factory1.tmx");
-        assertNotNull(this.urlMap);
-        //this.gameModel = new GameModelImpl(urlMap);
+    public void init() {
+        this.gameModel = new GameModelImpl(Constants.SOURCE_MAP);
         assertNotNull(this.gameModel);
     }
 
@@ -29,6 +29,39 @@ public class TestGameModel {
     public void testGetStationary() {
         List<List<Tile>> stationary = gameModel.getStationary();
         assertNotNull(stationary);
+        assertFalse(stationary.isEmpty());
+    }
+
+    @Test
+    public void testGetSaws() {
+        List<CircularSaw> saws = gameModel.getSaws();
+        assertNotNull(saws);
+        assertFalse(saws.isEmpty());
+    }
+
+    @Test
+    public void testGetPlatforms() {
+        List<Platform> platforms = gameModel.getPlatforms();
+        assertNotNull(platforms);
+        assertFalse(platforms.isEmpty());
+    }
+
+    @Test
+    public void testGetBandageGirl() {
+        BandageGirl bandageGirl = gameModel.getBandageGirl();
+        assertNotNull(bandageGirl);
+    }
+
+    @Test
+    public void testGetMeatBoy() {
+        MeatBoy meatBoy = gameModel.getMeatBoy();
+        assertNotNull(meatBoy);
+    }
+
+    @Test
+    public void testGetMeatBoyStartCoord() {
+        Point2D<Double, Double> startCoord = gameModel.getMeatBoyStartCoord();
+        assertNotNull(startCoord);
     }
 
     @Test
@@ -44,4 +77,3 @@ public class TestGameModel {
     }
 
 }
-
