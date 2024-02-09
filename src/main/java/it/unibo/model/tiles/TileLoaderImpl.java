5ce49eeb6f53784532bafdaba4358a9561809a26
Martin Tomassi <martin.tomassi@studio.unibo.it>
2024-02-09 20:39:18 +0100

    Fix checkstyle in TileLoader Implementation of TileLoader Interface

--- src/main/java/it/unibo/model/tiles/TileLoaderImpl.java
+++ src/main/java/it/unibo/model/tiles/TileLoaderImpl.java
@@ -11,27 +11,35 @@ import it.unibo.commons.Constants;
 import it.unibo.model.entity.obstacles.CircularSawImpl;
 import it.unibo.model.entity.obstacles.PlatformImpl;
 
+/**
+ * The TileLoaderImpl class is responsible for loading tiles and objects from a TMX file
+ * and populating the TileManager with the loaded data.
+ */
 public class TileLoaderImpl implements TileLoader {
 
-	private final TileManager tileManager;
+    private final TileManager tileManager;
     private final DocumentExtractor documentExtractor;
     private final int numColumns;
     private final int numRows;
 
-	/**
- 	 * Constructs a TileLoaderImpl instance with the specified TileManager.
- 	 * Initializes the TileLoaderImpl with the necessary information from the provided TileManager,
- 	 * including the number of rows, number of columns, and the XML document containing tile data.
- 	 *
- 	 * @param tileManager The TileManager providing information about the game level's tiles.
- 	 */
+    /**
+     * Constructs a TileLoaderImpl instance with the specified TileManager.
+     * Initializes the TileLoaderImpl with the necessary information from the provided TileManager,
+     * including the number of rows, number of columns, and the XML document containing tile data.
+     *
+     * @param tileManager The TileManager providing information about the game level's tiles.
+     * @param tmx The path to the TMX file containing tile data.
+     */
     public TileLoaderImpl(final TileManager tileManager, final String tmx) {
-		this.tileManager = tileManager;
+        this.tileManager = tileManager;
         this.documentExtractor = new DocumentExtractorImpl(tmx);
         this.numRows = documentExtractor.getNumRows();
         this.numColumns = documentExtractor.getNumColumns();
     }
 
+    /**
+     * Loads stationary tiles and objects from the TMX file.
+     */
     @Override
     public void load() {
         loadStationaryTiles();
@@ -39,33 +47,49 @@ public class TileLoaderImpl implements TileLoader {
         loadObjects("saws");
     }
 
+    /**
+     * Loads stationary tiles from the TMX file and populates the TileManager.
+     */
     private void loadStationaryTiles() {
         NodeList tileNodeList = documentExtractor.getElements("tile");
 
         IntStream.range(0, numRows)
-                .forEach(row -> IntStream.range(0, numColumns)
+                .forEach(
+                    row -> IntStream.range(0, numColumns)
                         .forEach(column -> {
                             int gidNumber = row * numColumns + column;
                             if (gidNumber < tileNodeList.getLength()) {
                                 Element tilesetElement = (Element) Objects.requireNonNull(tileNodeList.item(gidNumber));
-                                int idTile = Integer.parseInt(tilesetElement.getAttributes().getNamedItem("gid").getTextContent());
-    
+                                int idTile = Integer.parseInt(
+                                    tilesetElement.getAttributes().getNamedItem("gid").getTextContent());
+
                                 if (idTile > Constants.ID_TILE_NULL) {
                                     if (idTile == Constants.ID_TILE_BANDAGEGIRL) {
-                                        tileManager.getBandageGirl().setX(Double.valueOf(column * Constants.TILE_SIZE));
-                                        tileManager.getBandageGirl().setY(Double.valueOf(row * Constants.TILE_SIZE));
-                                        tileManager.getStationary().get(row).set(column, Optional.of(tileManager.getTiles().get(idTile - 1)));
+                                        tileManager.getBandageGirl().setX(
+                                            Double.valueOf(column * Constants.TILE_SIZE));
+                                        tileManager.getBandageGirl().setY(
+                                            Double.valueOf(row * Constants.TILE_SIZE));
+                                        tileManager.getStationary().get(row)
+                                            .set(column, Optional.of(tileManager.getTiles().get(idTile - 1)));
                                     } else if (idTile == Constants.ID_TILE_MEATBOY) {
-                                        tileManager.getMeatBoy().setX(Double.valueOf(column * Constants.TILE_SIZE));
-                                        tileManager.getMeatBoy().setY(Double.valueOf(row * Constants.TILE_SIZE));
+                                        tileManager.getMeatBoy().setX(
+                                            Double.valueOf(column * Constants.TILE_SIZE));
+                                        tileManager.getMeatBoy().setY(
+                                            Double.valueOf(row * Constants.TILE_SIZE));
                                     } else {
-                                        tileManager.getStationary().get(row).set(column, Optional.of(tileManager.getTiles().get(idTile - 1)));
+                                        tileManager.getStationary().get(row)
+                                            .set(column, Optional.of(tileManager.getTiles().get(idTile - 1)));
                                     }
                                 }
                             }
                         }));
     }
 
+    /**
+     * Loads objects from the TMX file and populates the TileManager.
+     *
+     * @param nameObjects The name of the objects to load ( "rectangle" , "saws" ).
+     */
     private void loadObjects(final String nameObjects) {
         NodeList objects = documentExtractor.getElements("objectgroup");
         IntStream.range(0, objects.getLength())
@@ -82,30 +106,30 @@ public class TileLoaderImpl implements TileLoader {
                             .forEach(objectElement -> {
                                 if (nameObjects.equals("saws")) { 
                                     this.tileManager.getSaws().add(
-								    new CircularSawImpl(
-									    Integer.parseInt(trim(objectElement.getAttribute("x"))),
-									    Integer.parseInt(trim(objectElement.getAttribute("y"))),
-									    Integer.parseInt(trim(objectElement.getAttribute("width")))));
+                                    new CircularSawImpl(
+                                        Integer.parseInt(trim(objectElement.getAttribute("x"))),
+                                        Integer.parseInt(trim(objectElement.getAttribute("y"))),
+                                        Integer.parseInt(trim(objectElement.getAttribute("width")))));
                                 } else {
                                     this.tileManager.getPlatforms().add(
-								    new PlatformImpl(
-									    Integer.parseInt(trim(objectElement.getAttribute("x"))),
-									    Integer.parseInt(trim(objectElement.getAttribute("y"))),
-									    Integer.parseInt(trim(objectElement.getAttribute("width"))),
-									    Integer.parseInt(trim(objectElement.getAttribute("height")))));
+                                    new PlatformImpl(
+                                        Integer.parseInt(trim(objectElement.getAttribute("x"))),
+                                        Integer.parseInt(trim(objectElement.getAttribute("y"))),
+                                        Integer.parseInt(trim(objectElement.getAttribute("width"))),
+                                        Integer.parseInt(trim(objectElement.getAttribute("height")))));
                                 }
                             });
                 });
     }
 
     /**
- 	 * Trims any extraneous space after a period in the given string.
- 	 *
- 	 * @param s The String to trim
- 	 * @return The trimmed String
- 	 */
-	private String trim(String s) {
-    	return s.contains(".") ? s.replaceAll("\\s+", "") : s;
-	}
+     * Trims any extraneous space after a period in the given string.
+     *
+     * @param s The String to trim
+     * @return The trimmed String
+     */
+    private String trim(final String s) {
+        return s.contains(".") ? s.replaceAll("\\s+", "") : s;
+    }
 
 }
