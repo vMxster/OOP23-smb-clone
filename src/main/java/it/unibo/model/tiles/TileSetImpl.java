d7053691e48a32bfa304fd66657d721b4fe852df
Martin Tomassi <martin.tomassi@studio.unibo.it>
2024-02-09 21:33:37 +0100

    Fix checkstyle in TileSet Implementation of TileSet Interface

--- src/main/java/it/unibo/model/tiles/TileSetImpl.java
+++ src/main/java/it/unibo/model/tiles/TileSetImpl.java
@@ -16,27 +16,38 @@ import org.xml.sax.SAXException;
 
 import it.unibo.commons.Constants;
 
+/**
+ * Implementation of the TileSet interface for parsing TMX files and extracting tiles.
+ */
 public class TileSetImpl implements TileSet {
 
-	private final String tmx;
-	private final List<Tile> tiles;
+    private final String tmx;
+    private final List<Tile> tiles;
 
-	/**
-	 * Constructs a new TileSet object by parsing a tmx file.
-	 * @param tmx The tmx file to parse
-	 */
-	public TileSetImpl(String tmx) {
-		this.tmx = tmx;
-		this.tiles = new ArrayList<>();
-		try {
+    /**
+     * Constructs a new TileSet object by parsing a TMX file.
+     *
+     * @param tmx The TMX file to parse.
+     */
+    public TileSetImpl(final String tmx) {
+        this.tmx = tmx;
+        this.tiles = new ArrayList<>();
+        try {
             read();
         } catch (ParserConfigurationException | SAXException | IOException e) {
             e.printStackTrace();
         }
-	}
+    }
 
-	@Override
-	public void read() throws ParserConfigurationException, SAXException, IOException {
+    /**
+     * Analyzes the TMX file to identify the appropriate sprite sheets.
+     *
+     * @throws ParserConfigurationException If a DocumentBuilder cannot be created which satisfies the configuration requested.
+     * @throws SAXException                 If any parse errors occur.
+     * @throws IOException                  If an I/O error occurs while reading the XML file.
+     */
+    @Override
+    public void read() throws ParserConfigurationException, SAXException, IOException {
     DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
     Document document = documentBuilder.parse(tmx);
     NodeList tileSetNodeList = document.getElementsByTagName("tileset");
@@ -45,34 +56,48 @@ public class TileSetImpl implements TileSet {
             .mapToObj(i -> (Element) tileSetNodeList.item(i))
             .forEach(tilesetElement -> {
                 try {
-					divideSpriteSheet(
-						Integer.parseInt(Objects.requireNonNull(tilesetElement.getElementsByTagName("image").item(0)).getAttributes().getNamedItem("width").getTextContent()),
-						Integer.parseInt(Objects.requireNonNull(tilesetElement.getElementsByTagName("image").item(0)).getAttributes().getNamedItem("height").getTextContent()),
-						Objects.requireNonNull(tilesetElement.getElementsByTagName("image").item(0)).getAttributes().getNamedItem("source").getTextContent());
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
+                    divideSpriteSheet(
+                        Integer.parseInt(Objects.requireNonNull(
+                            tilesetElement.getElementsByTagName("image").item(0))
+                                .getAttributes().getNamedItem("width").getTextContent()),
+                        Integer.parseInt(Objects.requireNonNull(
+                            tilesetElement.getElementsByTagName("image").item(0))
+                                .getAttributes().getNamedItem("height").getTextContent()),
+                        Objects.requireNonNull(
+                            tilesetElement.getElementsByTagName("image").item(0))
+                                .getAttributes().getNamedItem("source").getTextContent());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             });
-	}
+    }
 
+    /**
+     * Retrieves a list of tiles contained in the TileSet.
+     *
+     * @return A list of Tile objects representing the tiles in the TileSet.
+     */
+    @Override
+    public List<Tile> getTiles() {
+        return this.tiles;
+    }
 
-	@Override
-	public List<Tile> getTiles() {
-		return this.tiles;
-	}
+    /**
+     * Splits the sprite sheet into tiles of the appropriate size.
+     *
+     * @param width    The width of the tileset image.
+     * @param height   The height of the tileset image.
+     * @param srcImage The source image of the tileset.
+     * @throws IOException If an I/O error occurs.
+     */
+    private void divideSpriteSheet(final int width, final int height, final String srcImage) throws IOException {
+        IntStream.range(0, height / Constants.TILE_SIZE)
+                .boxed()
+                .flatMap(
+                    row -> IntStream.range(0, width / Constants.TILE_SIZE)
+                .mapToObj(
+                    column -> new TileImpl(column * Constants.TILE_SIZE, row * Constants.TILE_SIZE, srcImage)))
+                .forEach(tiles::add);
+    }
 
-	/**
-	 * Splits the SpriteSheet into tiles of the appropriate size.
-	 * @param w The width of the TileSet image
-	 * @param h The height of the TileSet image
-	 * @throws IOException
-	 */
-	private void divideSpriteSheet(final int width, final int height, final String srcImage) throws IOException {
-		IntStream.range(0, height / 20)
-				.boxed()
-				.flatMap(row -> IntStream.range(0, width / Constants.TILE_SIZE)
-				.mapToObj(column -> new TileImpl(column * Constants.TILE_SIZE, row * Constants.TILE_SIZE, srcImage)))
-				.forEach(tiles::add);
-	}
-	
 }
