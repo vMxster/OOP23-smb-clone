f4803d8da2c71dc9a6f34cb5e22bb7ec9e48732b
Martin Tomassi <martin.tomassi@studio.unibo.it>
2024-02-09 21:32:30 +0100

    Fix checkstyle in TileLoader Interface

--- src/main/java/it/unibo/model/tiles/TileLoader.java
+++ src/main/java/it/unibo/model/tiles/TileLoader.java
@@ -5,7 +5,7 @@ package it.unibo.model.tiles;
  */
 public interface TileLoader {
 
-	/**
+    /**
      * Loads stationary tiles, platforms and circular saws from an XML document.
      * This method delegates to specific loading methods based on the type of tiles to be loaded.
      */
