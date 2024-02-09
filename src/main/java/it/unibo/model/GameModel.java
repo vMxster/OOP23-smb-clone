4b31f2718b1d8172be3cd70cf59c2b4bc7130841
Martin Tomassi <martin.tomassi@studio.unibo.it>
2024-02-09 21:00:45 +0100

    Fix checkstyle in GameModel Interface

--- src/main/java/it/unibo/model/GameModel.java
+++ src/main/java/it/unibo/model/GameModel.java
@@ -14,8 +14,9 @@ import it.unibo.model.tiles.Tile;
 /**
  * The GameModel interface represents the ModelManager of the Game SuperMeatBoy, providing methods to retrieve
  * information about the tiles in the game grid.
- * The game grid consists of foreground, background, and stationary tiles, organized in a
- * two-dimensional structure where each inner list corresponds to a row, and the elements
+ * The game grid consists of background, saws, platforms, meatboy, bandagegirl
+ * and stationary tiles, organized in a two-dimensional structure
+ * where each inner list corresponds to a row, and the elements
  * within the inner lists represent tiles in that specific row.
  */
 public interface GameModel {
@@ -37,7 +38,7 @@ public interface GameModel {
      * @return The List of CircularSaws parsed from the TMX file.
      */
     List<CircularSaw> getSaws();
-    
+
     /**
      * Returns the List of Platforms parsed from the TMX file.
      *
@@ -64,22 +65,27 @@ public interface GameModel {
      *
      * @return The Starting Coordinates of MeatBoy.
      */
-    Point2D<Double,Double> getMeatBoyStartCoord();
+    Point2D<Double, Double> getMeatBoyStartCoord();
 
-     /**
+    /**
      * Retrieves the total number of rows in the grid of tiles.
      *
      * @return The number of rows in the grid.
      */
     int getNumRows();
 
-     /**
+    /**
      * Retrieves the total number of columns in the grid of tiles.
      *
      * @return The number of columns in the grid.
      */
     int getNumCols();
 
+    /**
+     * Retrieves the collision handler associated with the game model.
+     *
+     * @return The collision handler.
+     */
     CollisionHandler getCollisionHandler();
-    
+
 }
