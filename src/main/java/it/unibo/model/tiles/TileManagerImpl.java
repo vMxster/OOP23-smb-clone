ae03f337d66ee67d8ea24fbe5bd50f85f63a0e58
Martin Tomassi <martin.tomassi@studio.unibo.it>
2024-02-09 20:43:31 +0100

    Fix checkstyle in TileManager Implementation of TileManager Interface

--- src/main/java/it/unibo/model/tiles/TileManagerImpl.java
+++ src/main/java/it/unibo/model/tiles/TileManagerImpl.java
@@ -12,89 +12,133 @@ import it.unibo.model.entity.player.MeatBoyImpl;
 import it.unibo.model.entity.target.BandageGirl;
 import it.unibo.model.entity.target.BandageGirlImpl;
 
-public class TileManagerImpl implements TileManager{
+/**
+ * The TileManagerImpl class implements the TileManager interface and provides
+ * functionality to manage tiles, platforms, circular saws, and player entities.
+ */
+public class TileManagerImpl implements TileManager {
 
-	private final List<List<Optional<Tile>>> stationary;
-	private final List<Platform> platforms;
-	private final List<CircularSaw> circularSaws;
-	private final TileSet tileSet;
-	private final List<Tile> tiles;
-	private final MeatBoy meatBoy;
-	private final BandageGirl bandageGirl;
-	private final TileLoader tileLoader;
-	private final DocumentExtractor documentExtractor;
-	private final int numRows;
-	private final int numColumns;
+    private final List<List<Optional<Tile>>> stationary;
+    private final List<Platform> platforms;
+    private final List<CircularSaw> circularSaws;
+    private final TileSet tileSet;
+    private final List<Tile> tiles;
+    private final MeatBoy meatBoy;
+    private final BandageGirl bandageGirl;
+    private final TileLoader tileLoader;
+    private final DocumentExtractor documentExtractor;
+    private final int numRows;
+    private final int numColumns;
 
-	/**
- 	 * Constructs a new TileManager object by parsing a specified tmx file.
- 	 *
- 	 * @param urlMap The URL of the tmx file to read and create the TileManager from.
- 	 */
-	public TileManagerImpl(final String tmx) {
-		this.platforms = new ArrayList<Platform>();
-		this.circularSaws = new ArrayList<CircularSaw>();
-		this.stationary = new ArrayList<>();
-		this.tileSet = new TileSetImpl(tmx);
-		this.tiles = tileSet.getTiles();
-		this.meatBoy = new MeatBoyImpl(0, 0);
-		this.bandageGirl = new BandageGirlImpl(0, 0);
-		this.documentExtractor = new DocumentExtractorImpl(tmx);
-		this.tileLoader = new TileLoaderImpl(this, tmx);
-		this.numRows = documentExtractor.getNumRows();
-		this.numColumns = documentExtractor.getNumColumns();
-		init();
-		this.tileLoader.load();
-	}
+    /**
+     * Constructs a new TileManager object by parsing a specified TMX file.
+     *
+     * @param tmx The path to the TMX file.
+     */
+    public TileManagerImpl(final String tmx) {
+        this.platforms = new ArrayList<Platform>();
+        this.circularSaws = new ArrayList<CircularSaw>();
+        this.stationary = new ArrayList<>();
+        this.tileSet = new TileSetImpl(tmx);
+        this.tiles = tileSet.getTiles();
+        this.meatBoy = new MeatBoyImpl(0, 0);
+        this.bandageGirl = new BandageGirlImpl(0, 0);
+        this.documentExtractor = new DocumentExtractorImpl(tmx);
+        this.tileLoader = new TileLoaderImpl(this, tmx);
+        this.numRows = documentExtractor.getNumRows();
+        this.numColumns = documentExtractor.getNumColumns();
+        init();
+        this.tileLoader.load();
+    }
 
-	@Override
-	public List<Platform> getPlatforms(){
-		return this.platforms;
-	}
+    /**
+     * Returns the list of platforms.
+     *
+     * @return The list of platforms.
+     */
+    @Override
+    public List<Platform> getPlatforms() {
+        return this.platforms;
+    }
 
-	@Override
-	public List<CircularSaw> getSaws(){
-		return this.circularSaws;
-	}
+    /**
+     * Returns the list of circular saws.
+     *
+     * @return The list of circular saws.
+     */
+    @Override
+    public List<CircularSaw> getSaws() {
+        return this.circularSaws;
+    }
 
-	@Override
-	public int getNumCols(){
-		return this.numColumns;
-	}
+    /**
+     * Returns the number of columns in the tile map.
+     *
+     * @return The number of columns.
+     */
+    @Override
+    public int getNumCols() {
+        return this.numColumns;
+    }
 
-	@Override
-	public int getNumRows(){
-		return this.numRows;
-	}
+    /**
+     * Returns the number of rows in the tile map.
+     *
+     * @return The number of rows.
+     */
+    @Override
+    public int getNumRows() {
+        return this.numRows;
+    }
 
-	@Override
-	public MeatBoy getMeatBoy() {
-		return this.meatBoy;
-	}
+    /**
+     * Returns the MeatBoy entity.
+     *
+     * @return The MeatBoy entity.
+     */
+    @Override
+    public MeatBoy getMeatBoy() {
+        return this.meatBoy;
+    }
 
-	@Override
-	public BandageGirl getBandageGirl() {
-		return this.bandageGirl;
-	}
+    /**
+     * Returns the BandageGirl entity.
+     *
+     * @return The BandageGirl entity.
+     */
+    @Override
+    public BandageGirl getBandageGirl() {
+        return this.bandageGirl;
+    }
 
-	@Override
-	public List<List<Optional<Tile>>> getStationary() {
-		return this.stationary;
-	}
+    /**
+     * Returns the list of stationary tiles.
+     *
+     * @return The list of stationary tiles.
+     */
+    @Override
+    public List<List<Optional<Tile>>> getStationary() {
+        return this.stationary;
+    }
 
-	@Override
-	public List<Tile> getTiles() {
-		return this.tiles;
-	}
+    /**
+     * Returns the list of all tiles.
+     *
+     * @return The list of all tiles.
+     */
+    @Override
+    public List<Tile> getTiles() {
+        return this.tiles;
+    }
 
-	/**
- 	 * Initializes the stationary list.
- 	 * This method is called during the construction of the TileManager object.
- 	 */
-	private void init() {
-		for (int i = 0 ; i<this.numRows ; i++) {
-			stationary.add(new ArrayList<>(Collections.nCopies(this.numColumns, Optional.empty())));
-		}
-	}
+    /**
+     * Initializes the stationary list.
+     * This method is called during the construction of the TileManager object.
+     */
+    private void init() {
+        for (int i = 0; i < this.numRows; i++) {
+            stationary.add(new ArrayList<>(Collections.nCopies(this.numColumns, Optional.empty())));
+        }
+    }
 
 }
