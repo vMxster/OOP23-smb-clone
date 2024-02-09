8216672feb31ede208d003097d533a671e74757b
Martin Tomassi <martin.tomassi@studio.unibo.it>
2024-02-09 22:08:07 +0100

    Fix checkstyle in the Test of TileLoader and Modified its Tests

--- src/test/java/it/unibo/model/tiles/TestTileLoader.java
+++ src/test/java/it/unibo/model/tiles/TestTileLoader.java
@@ -1,47 +1,49 @@
 package it.unibo.model.tiles;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import it.unibo.commons.Constants;
 
+/**
+ * JUnit tests for the TileLoader class.
+ */
 public class TestTileLoader {
 
     private TileManager tileManager;
+    private DocumentExtractor documentExtractor;
     private TileLoaderImpl tileLoader;
 
+    /**
+     * Initializes the test environment before each test method is executed.
+     */
     @BeforeEach
     public void init() {
         this.tileManager = new TileManagerImpl(Constants.SOURCE_MAP);
         assertNotNull(this.tileManager);
-        this.tileLoader = new TileLoaderImpl(tileManager);
+        this.tileLoader = new TileLoaderImpl(tileManager, Constants.SOURCE_MAP);
         assertNotNull(this.tileManager);
+        this.documentExtractor = new DocumentExtractorImpl(Constants.SOURCE_MAP);
+        assertNotNull(this.documentExtractor);
     }
 
+    /**
+     * Tests the {@link TileLoaderImpl#load()} method.
+     */
     @Test
-    public void testLoadStationaryTiles() {
-        this.tileLoader.loadStationaryTiles();
+    public void testLoad() {
+        this.tileLoader.load();
         assertNotNull(this.tileManager.getStationary());
         assertFalse(this.tileManager.getStationary().isEmpty());
+        assertNotNull(this.tileManager.getSaws());
+        assertFalse(this.tileManager.getSaws().isEmpty());
+        assertNotNull(this.tileManager.getPlatforms());
+        assertFalse(this.tileManager.getPlatforms().isEmpty());
         assertNotNull(this.tileManager.getBandageGirl());
         assertNotNull(this.tileManager.getMeatBoy());
     }
 
-    @Test
-    public void testLoadPlatforms() {
-        this.tileLoader.loadPlatforms();
-        assertNotNull(this.tileManager.getPlatforms());
-        assertFalse(this.tileManager.getPlatforms().isEmpty());
-    }
-
-    @Test
-    public void testLoadCircularSaws() {
-        tileLoader.loadCircularSaws();
-        assertNotNull(this.tileManager.getSaws());
-        assertFalse(this.tileManager.getSaws().isEmpty());
-    }
-
 }
-
