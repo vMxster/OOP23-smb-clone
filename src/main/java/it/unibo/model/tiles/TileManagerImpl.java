20740bf77ff4e93d1ecde6290e9e2cf5b9e5c697
Martin Tomassi <martin.tomassi@studio.unibo.it>
2024-02-09 17:10:58 +0100

    Modified TileManager Implementation of TileManager Interface - Removed the part where the TileManager is in charge of getting the Document and taking the information from it, delegated to the DocumentExtractor , Removed loadMap Method and Document Getter , Modified the Init method that now fills the Stationary with empty Optional instead of filling it with null Tile instances

--- src/main/java/it/unibo/model/tiles/TileManagerImpl.java
+++ src/main/java/it/unibo/model/tiles/TileManagerImpl.java
@@ -1,16 +1,9 @@
 package it.unibo.model.tiles;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import org.w3c.dom.Document;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
-import org.w3c.dom.Element;
+import java.util.Optional;
 
 import it.unibo.model.entity.obstacles.CircularSaw;
 import it.unibo.model.entity.obstacles.Platform;
@@ -21,19 +14,17 @@ import it.unibo.model.entity.target.BandageGirlImpl;
 
 public class TileManagerImpl implements TileManager{
 
-	private final List<List<Tile>> stationary;
+	private final List<List<Optional<Tile>>> stationary;
 	private final List<Platform> platforms;
 	private final List<CircularSaw> circularSaws;
 	private final TileSet tileSet;
 	private final List<Tile> tiles;
 	private final MeatBoy meatBoy;
 	private final BandageGirl bandageGirl;
-	private final String tmx;
-	private TileLoader tileLoader;
-	private DocumentBuilder builder;
-	private Document document;
-	private int numRows;
-	private int numColumns;
+	private final TileLoader tileLoader;
+	private final DocumentExtractor documentExtractor;
+	private final int numRows;
+	private final int numColumns;
 
 	/**
  	 * Constructs a new TileManager object by parsing a specified tmx file.
@@ -48,32 +39,12 @@ public class TileManagerImpl implements TileManager{
 		this.tiles = tileSet.getTiles();
 		this.meatBoy = new MeatBoyImpl(0, 0);
 		this.bandageGirl = new BandageGirlImpl(0, 0);
-		this.tmx = tmx;
-	
-		try {
-			this.builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-			this.document = builder.parse(this.tmx);
-	
-			NodeList mapAttributes = document.getElementsByTagName("map");
-			if (mapAttributes.getLength() > 0) {
-				Element mapElement = (Element) mapAttributes.item(0);
-				this.numRows = Integer.parseInt(mapElement.getAttribute("height"));
-				this.numColumns = Integer.parseInt(mapElement.getAttribute("width"));
-			}
-	
-			this.tileLoader = new TileLoaderImpl(this);
-			init();
-			loadMap();
-		} catch (SAXException | ParserConfigurationException | IOException exception) {
-			exception.printStackTrace();
-		}
-	}
-
-	@Override
-	public void loadMap() {
-		this.tileLoader.loadStationaryTiles();
-		this.tileLoader.loadPlatforms();
-		this.tileLoader.loadCircularSaws();
+		this.documentExtractor = new DocumentExtractorImpl(tmx);
+		this.tileLoader = new TileLoaderImpl(this, tmx);
+		this.numRows = documentExtractor.getNumRows();
+		this.numColumns = documentExtractor.getNumColumns();
+		init();
+		this.tileLoader.load();
 	}
 
 	@Override
@@ -107,12 +78,7 @@ public class TileManagerImpl implements TileManager{
 	}
 
 	@Override
-	public Document getDocument() {
-		return this.document;
-	}
-
-	@Override
-	public List<List<Tile>> getStationary() {
+	public List<List<Optional<Tile>>> getStationary() {
 		return this.stationary;
 	}
 
@@ -127,7 +93,7 @@ public class TileManagerImpl implements TileManager{
  	 */
 	private void init() {
 		for (int i = 0 ; i<this.numRows ; i++) {
-			stationary.add(new ArrayList<>(Collections.nCopies(this.numColumns, new TileImpl(0, 0, "null"))));
+			stationary.add(new ArrayList<>(Collections.nCopies(this.numColumns, Optional.empty())));
 		}
 	}
 
