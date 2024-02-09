58ced96f8cea6fd72f4684e83bb5b891237e09e7
Martin Tomassi <martin.tomassi@studio.unibo.it>
2024-02-09 21:32:57 +0100

    Fix checkstyle in TileSet Interface

--- src/main/java/it/unibo/model/tiles/TileSet.java
+++ src/main/java/it/unibo/model/tiles/TileSet.java
@@ -6,23 +6,25 @@ import java.io.IOException;
 import java.util.List;
 
 /**
- * An interface representing a collection of tiles obtained from a tmx file.
+ * An interface representing a collection of tiles obtained from a TMX file.
  * Implementing classes should provide methods to access and manipulate the TileSet.
  */
 public interface TileSet {
 
     /**
-	 * Analyzes the tmx file to identify the appropriate SpriteSheets.
-	 * @throws XPathExpressionException
-	 * @throws SAXException
-	 * @throws ParserConfigurationException
-	 * @throws IOException
-	 */
+     * Analyzes the TMX file to identify the appropriate sprite sheets.
+     *
+     * @throws ParserConfigurationException If a DocumentBuilder cannot be created which satisfies the configuration requested.
+     * @throws SAXException                 If any parse errors occur.
+     * @throws IOException                  If an I/O error occurs while reading the XML file.
+     */
     void read() throws ParserConfigurationException, SAXException, IOException;
 
-	/**
-	 * @return a list of Tiles.
-	 */
-	List<Tile> getTiles();
+    /**
+     * Retrieves a list of tiles contained in the TileSet.
+     *
+     * @return A list of Tile objects representing the tiles in the TileSet.
+     */
+    List<Tile> getTiles();
 
 }
