e5f0bd5be286b3c16cc1772f3df66c9761bb4cd4
Nicola Graziotin <nicola.graziotin@studio.unibo.it>
2024-02-10 15:50:11 +0100

    Fix checkstyle issues in MeatBoy

--- src/main/java/it/unibo/model/entity/player/MeatBoy.java
+++ src/main/java/it/unibo/model/entity/player/MeatBoy.java
@@ -3,11 +3,23 @@ package it.unibo.model.entity.player;
 import it.unibo.model.entity.Entity;
 import it.unibo.model.hitbox.RectangleHitbox;
 
+/**
+ * Represents a specific type of entity in a game or simulation context, specifically a MeatBoy.
+ * It extends the {@link Entity} interface, specifying a hitbox of type {@link RectangleHitbox}.
+ */
 public interface MeatBoy extends Entity<RectangleHitbox> {
 
-    static final double SPEED = 4;
-
+    /**
+     * Retrieves the speed multiplier associated with this entity.
+     *
+     * @return The speed multiplier value.
+     */
     double getSpeedMul();
 
+    /**
+     * Sets the speed multiplier associated with this entity to the specified value.
+     *
+     * @param speedMul The new value for the speed multiplier.
+     */
     void setSpeedMul(double speedMul);
 }
