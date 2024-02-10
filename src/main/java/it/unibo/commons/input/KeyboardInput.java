c27eb3c17a59aedb476b98f9aedf55d81c8ae6d9
Nicola Graziotin <nicola.graziotin@studio.unibo.it>
2024-02-10 16:01:25 +0100

    Fix checkstyle issues in KeyboardInput

--- src/main/java/it/unibo/commons/input/KeyboardInput.java
+++ src/main/java/it/unibo/commons/input/KeyboardInput.java
@@ -5,23 +5,40 @@ import java.awt.event.KeyEvent;
 
 import it.unibo.view.panel.GamePanel;
 
-public class KeyboardInput extends KeyAdapter{
+/**
+ * This class represents a keyboard input handler that extends the KeyAdapter class.
+ * It provides methods to handle keyboard input events.
+ */
+public class KeyboardInput extends KeyAdapter {
 
-    GamePanel panel;
-    
-    public KeyboardInput(GamePanel panel) {
+    private GamePanel panel;
+
+    /**
+     * Constructs a KeyboardInput object with the specified GamePanel.
+     * 
+     * @param panel The GamePanel to which this keyboard input handler is associated.
+     */
+    public KeyboardInput(final GamePanel panel) {
         this.panel = panel;
     }
 
+    /**
+     * Invoked when a key is pressed while the associated component has focus.
+     * 
+     * @param e The KeyEvent object containing information about the key press event.
+     */
     @Override
-    public void keyPressed(KeyEvent e) {
-        panel.keyPressed(e);
+    public void keyPressed(final KeyEvent e) {
+        this.panel.keyPressed(e);
     }
 
+    /**
+     * Invoked when a key is released while the associated component has focus.
+     *
+     * @param e The KeyEvent object containing information about the key release event.
+     */
     @Override
-    public void keyReleased(KeyEvent e) {
-        panel.keyReleased(e);
+    public void keyReleased(final KeyEvent e) {
+        this.panel.keyReleased(e);
     }
-
-    
 }
