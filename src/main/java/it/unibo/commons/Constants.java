376023b70559d41b5fe7045dd2a35f3321c33212
Martin Tomassi <martin.tomassi@studio.unibo.it>
2024-02-10 13:04:10 +0100

    Fix Checkstyle issues of Constants Utility Class and Removed Tile Constants

--- src/main/java/it/unibo/commons/Constants.java
+++ src/main/java/it/unibo/commons/Constants.java
@@ -1,13 +1,27 @@
 package it.unibo.commons;
 
-public class Constants {
-    
+/**
+ * This class provides constants used throughout the application.
+ */
+public final class Constants {
+
+    /** The size of a tile in pixels. */
     public static final int TILE_SIZE = 20;
+
+    /** The width of the screen. */
     public static final int SW = 800;
+
+    /** The height of the screen. */
     public static final int SH = 600;
-    public static final int ID_TILE_BANDAGEGIRL = 902;
-    public static final int ID_TILE_MEATBOY = 901;
-    public static final int ID_TILE_NULL = 0;
+
+    /** The source map file path. */
     public static final String SOURCE_MAP = "file:./src/main/resources/factory1.tmx";
+
+    /**
+     * Private constructor to prevent instantiation.
+     * Utility classes should not be instantiated.
+     */
+    private Constants() {
+    }
 
 }
