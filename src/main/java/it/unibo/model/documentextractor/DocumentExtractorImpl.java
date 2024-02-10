f0527cec01e9151a1eb8ef97f8cc569ee6b6ad64
Martin Tomassi <martin.tomassi@studio.unibo.it>
2024-02-10 13:19:14 +0100

    Fix PMD issues of DocumentExtractor Implementation of DocumentExtractor Interface

--- src/main/java/it/unibo/model/documentextractor/DocumentExtractorImpl.java
+++ src/main/java/it/unibo/model/documentextractor/DocumentExtractorImpl.java
@@ -1,14 +1,15 @@
 package it.unibo.model.documentextractor;
 
 import java.io.IOException;
+import java.util.logging.Logger;
 
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
 
 /**
  * Implementation of the DocumentExtractor interface that extracts information from a TMX document.
@@ -33,13 +34,14 @@ public class DocumentExtractorImpl implements DocumentExtractor {
     private void extractDocument() {
         try {
             this.document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(this.tmx);
-            NodeList mapAttributes = this.getElements("map");
+            final NodeList mapAttributes = this.document.getElementsByTagName("map");
             if (mapAttributes.getLength() > 0) {
                 this.numRows = Integer.parseInt(((Element) mapAttributes.item(0)).getAttribute("height"));
                 this.numColumns = Integer.parseInt(((Element) mapAttributes.item(0)).getAttribute("width"));
             }
         } catch (SAXException | IOException | ParserConfigurationException e) {
-            e.printStackTrace();
+            Logger.getLogger(DocumentExtractorImpl.class.getName())
+                .severe("An error occurred while extracting document: " + e.getMessage());
         }
     }
 
