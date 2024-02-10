3b1488774668be0a5a68ec710d1022f7df193c7b
Marcatelli Francesco <francesco.marcatelli@studio.unibo.it>
2024-02-10 15:57:30 +0100

    Fix checkstyle error and added Javadoc in CollisionHandlerImpl

--- src/main/java/it/unibo/model/collision/CollisionHandlerImpl.java
+++ src/main/java/it/unibo/model/collision/CollisionHandlerImpl.java
@@ -2,20 +2,37 @@ package it.unibo.model.collision;
 
 import it.unibo.model.GameModel;
 
+/**
+ * Implementation of the CollisionHandler interface responsible for handling collisions
+ * and updating the game model accordingly.
+ */
 public class CollisionHandlerImpl implements CollisionHandler {
     private CollisionChecker collisionChecker;
     private GameModel gameModel;
 
-    public CollisionHandlerImpl(GameModel gameModel) {
+    /**
+     * Constructs the CollisionHandlerImpl with the given GameModel.
+     * 
+     * @param gameModel the game model to be used for collision handling
+     */
+    public CollisionHandlerImpl(final GameModel gameModel) {
         this.gameModel = gameModel;
         this.collisionChecker = new CollisionCheckerImpl(this);
     }
 
+    /**
+     * Retrieves the associated GameModel.
+     * 
+     * @return the GameModel associated with this collision handler
+     */
     @Override
     public GameModel getGameModel() {
         return this.gameModel;
     }
 
+    /**
+     * Checks collisions and updates the game model based on the collision state.
+     */
     @Override
     public void check() {
         if (collisionChecker.isInWindow() == CollisionChecker.CollisionState.FALL) {
@@ -31,16 +48,29 @@ public class CollisionHandlerImpl implements CollisionHandler {
         }
     }
 
+    /**
+     * Passes the key code to the CollisionChecker for moving MeatBoy.
+     * 
+     * @param keyCode the key code representing the pressed key
+     */
     @Override
-    public void moveMeatBoy(int keyCode) {
+    public void moveMeatBoy(final int keyCode) {
         this.collisionChecker.moveMeatBoy(keyCode);
     }
 
+    /**
+     * Passes the key code to the CollisionChecker to stop MeatBoy's movement.
+     * 
+     * @param keyCode the key code representing the released key
+     */
     @Override
-    public void stopMovingMeatBoy(int keyCode) {
+    public void stopMovingMeatBoy(final int keyCode) {
         this.collisionChecker.stopMovingMeatBoy(keyCode);
     }
 
+    /**
+     * Updates MeatBoy's position using the CollisionChecker.
+     */
     @Override
     public void updateMeatBoy() {
         this.collisionChecker.updateMeatBoy();
