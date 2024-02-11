63baed2d752cbde792f21fcdbdfe7d2c5a428b3b
Martin Tomassi <martin.tomassi@studio.unibo.it>
2024-02-11 19:01:34 +0100

    Modified ImageRendererManager Implementation of ImageRendererManager Interface - Added ImageLoader field and Modified render Method

--- src/main/java/it/unibo/view/imagerenderer/manager/ImageRendererManagerImpl.java
+++ src/main/java/it/unibo/view/imagerenderer/manager/ImageRendererManagerImpl.java
@@ -2,22 +2,24 @@ package it.unibo.view.imagerenderer.manager;
 
 import java.awt.image.BufferedImage;
 import java.util.List;
-import java.util.Optional;
 
-import it.unibo.model.entity.obstacles.CircularSaw;
-import it.unibo.model.tiles.Tile;
+import it.unibo.controller.GameController;
 import it.unibo.view.imagerenderer.gameobjects.ImageRendererSaws;
 import it.unibo.view.imagerenderer.gameobjects.ImageRendererSawsImpl;
 import it.unibo.view.imagerenderer.gameobjects.ImageRendererStationary;
 import it.unibo.view.imagerenderer.gameobjects.ImageRendererStationaryImpl;
+import it.unibo.view.imagerenderer.loader.ImageLoader;
+import it.unibo.view.imagerenderer.loader.ImageLoaderImpl;
 
 /**
  * The ImageRendererManager class is responsible for managing the rendering of the game map.
  */
 public class ImageRendererManagerImpl implements ImageRendererManager {
 
-    private final ImageRendererSaws imageRendererGameObjects;
+    private final GameController gameController;
+    private final ImageRendererSaws imageRendererSaws;
     private final ImageRendererStationary imageRendererStationary;
+    private final ImageLoader imageLoader;
 
     /**
      * Constructs an ImageRendererManager with the specified number of rows and columns for the game map.
@@ -25,23 +27,27 @@ public class ImageRendererManagerImpl implements ImageRendererManager {
      * @param numRows    The number of rows in the game map.
      * @param numColumns The number of columns in the game map.
      */
-    public ImageRendererManagerImpl(final int numRows, final int numColumns) {
-        this.imageRendererGameObjects = new ImageRendererSawsImpl(numRows, numColumns);
-        this.imageRendererStationary = new ImageRendererStationaryImpl(numRows, numColumns);
+    public ImageRendererManagerImpl(final GameController gameController) {
+        this.gameController = gameController;
+        this.imageRendererSaws = new ImageRendererSawsImpl(
+            this.gameController.getNumRows(), this.gameController.getNumCols());
+        this.imageRendererStationary = new ImageRendererStationaryImpl(
+            this.gameController.getNumRows(), this.gameController.getNumCols());
+        this.imageLoader = new ImageLoaderImpl();
     }
 
     /**
-     * Manage the rendering of circular saws and stationary tiles into a list of BufferedImages.
+     * Manage the rendering of circular Saws, Stationary Tiles, Background and MeatBoy into a list of BufferedImages.
      *
-     * @param saws       The list of CircularSaw objects to render.
-     * @param stationary The 2D list of Optional Tile objects representing the stationary tiles.
      * @return A list of BufferedImages representing the rendered game elements.
      */
     @Override
-    public List<BufferedImage> render(final List<CircularSaw> saws, final List<List<Optional<Tile>>> stationary) {
+    public List<BufferedImage> render() {
         return List.of(
-            this.imageRendererGameObjects.render(saws),
-            this.imageRendererStationary.render(stationary));
+            this.imageRendererSaws.render(this.gameController.getSaws()),
+            this.imageRendererStationary.render(this.gameController.getStationary()),
+            this.imageLoader.load(ImageType.BACKGROUND),
+            this.imageLoader.load(ImageType.MEAT_BOY));
     }
 
 }
