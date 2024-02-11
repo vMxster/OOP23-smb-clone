6f4dee6a8e68ceeb3b37d2ae9513315fc094f591
Martin Tomassi <martin.tomassi@studio.unibo.it>
2024-02-11 17:56:36 +0100

    Added ImageRendererStationary Interface

---
+++ src/main/java/it/unibo/view/imagerenderer/stationary/ImageRendererStationary.java
@@ -0,0 +1,23 @@
+package it.unibo.view.imagerenderer.stationary;
+
+import java.awt.image.BufferedImage;
+import java.util.List;
+import java.util.Optional;
+
+import it.unibo.model.tiles.Tile;
+
+/**
+ * The ImageRendererStationary interface defines methods for rendering stationary tiles in a game as a BufferedImage.
+ */
+public interface ImageRendererStationary {
+
+    /**
+     * Renders the stationary tiles represented by the provided 2D List of Optional Tile objects
+     * into a BufferedImage.
+     *
+     * @param stationary A 2D List of Optional Tile objects representing the stationary tiles.
+     * @return A BufferedImage representing the stationary tiles.
+     */
+    BufferedImage render(List<List<Optional<Tile>>> stationary);
+
+}
