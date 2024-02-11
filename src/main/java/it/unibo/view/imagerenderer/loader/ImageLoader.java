a8669f2535c882e6bf406604bba81eb8c5e3dd84
Martin Tomassi <martin.tomassi@studio.unibo.it>
2024-02-11 19:02:02 +0100

    Added ImageLoader Interface

---
+++ src/main/java/it/unibo/view/imagerenderer/loader/ImageLoader.java
@@ -0,0 +1,20 @@
+package it.unibo.view.imagerenderer.loader;
+
+import java.awt.image.BufferedImage;
+
+import it.unibo.view.imagerenderer.manager.ImageType;
+
+/**
+ * The ImageLoader interface provides functionality to load images based on the specified ImageType.
+ */
+public interface ImageLoader {
+    
+    /**
+     * Loads the image associated with the specified ImageType.
+     *
+     * @param imageType The type of image to load.
+     * @return The loaded BufferedImage.
+     */
+    BufferedImage load(ImageType imageType);
+
+}
