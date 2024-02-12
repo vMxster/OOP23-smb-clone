ec6a613e614a50d1ceb5241c1de82fe2b507d5fb
Marcatelli Francesco <francesco.marcatelli@studio.unibo.it>
2024-02-12 17:07:22 +0100

    Added enum PanelType in GameWindow and added as parameter in switchPanel

--- src/main/java/it/unibo/view/window/GameWindow.java
+++ src/main/java/it/unibo/view/window/GameWindow.java
@@ -6,6 +6,7 @@ package it.unibo.view.window;
  */
 public interface GameWindow {
 
+    enum PanelType { GAME, MENU, SCOREBOARD }
     /**
      * Paints the content of the game window.
      * This method is responsible for updating and rendering the content of the game window.
@@ -30,9 +31,9 @@ public interface GameWindow {
      * This method set visible the current panel.
      */
     void setPanelVisible();
+
     /**
      * This method is responsible for switching between different panels within the game window.
      */
-    void switchPanel();
-
+    void switchPanel(PanelType type);
 }
