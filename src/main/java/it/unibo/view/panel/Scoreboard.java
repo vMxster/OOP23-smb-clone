22a8955f513944651f7560fe22deac5fc837af36
Mattia <mattia.galli9@studio.unibo.it>
2024-02-12 18:57:00 +0100

    Implemented backButton in Scoreboard.java

--- src/main/java/it/unibo/view/panel/Scoreboard.java
+++ src/main/java/it/unibo/view/panel/Scoreboard.java
@@ -8,7 +8,9 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import it.unibo.view.window.GameWindowSwing;
 import it.unibo.controller.GameController;
+import it.unibo.view.window.GameWindow.PanelType;
 /**
  * The Scoreboard class represents the panel where the User can view statistic after passing the level.
  * It extends JPanel and provides the number of deaths and the time that the User managed to pass the level.
@@ -24,17 +26,19 @@ public class Scoreboard extends JPanel  {
     /**
      * 
      * @param controller
+     * @param window
      */
-    public Scoreboard(final GameController controller) {
+    public Scoreboard(final GameController controller, final GameWindowSwing window) {
         this.setLayout(new GridLayout(4, 1));
         label = new JLabel("LEVEL 1");
-        deathsField = new JLabel("DEATHS: " + this.deaths);
-        timeRecordField = new JLabel("TIME: ");
+        deathsField = new JLabel("TOTAL DEATHS: " + this.deaths);
+        timeRecordField = new JLabel("RECORD TIME: ");
         backButton = new JButton("BACK");
 
         backButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(final ActionEvent e) {
+                window.switchPanel(PanelType.MENU);
             }
         });
 
@@ -44,12 +48,14 @@ public class Scoreboard extends JPanel  {
         this.add(backButton);
     }
 
-    
 
-    private void updateDeaths() {
+    
+    public void updateDeaths() {
+        deaths++;
+        System.out.println(deaths);
     }
 
-    private void updateTimeRecord() {
+    public void updateTimeRecord() {
 
     }
 }
