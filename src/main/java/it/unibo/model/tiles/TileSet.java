960055821648267cf7f9fef4ba6023947b7ce778
Martin Tomassi <martin.tomassi@studio.unibo.it>
2024-02-13 13:22:53 +0100

    Modified TileSet Interface - Removed parseTMXFile Method

--- src/main/java/it/unibo/model/tiles/TileSet.java
+++ src/main/java/it/unibo/model/tiles/TileSet.java
@@ -1,8 +1,5 @@
 package it.unibo.model.tiles;
 
-import javax.xml.parsers.ParserConfigurationException;
-import org.xml.sax.SAXException;
-import java.io.IOException;
 import java.util.List;
 
 /**
@@ -17,14 +14,5 @@ public interface TileSet {
      * @return A list of Tile objects extracted from the TMX file.
      */
     List<Tile> read();
-
-    /**
-     * Analyzes the TMX file to identify the appropriate sprite sheets. 
-     *
-     * @throws ParserConfigurationException If a DocumentBuilder cannot be created which satisfies the configuration requested.
-     * @throws SAXException                 If any parse errors occur.
-     * @throws IOException                  If an I/O error occurs while reading the XML file.
-     */
-    void parseTMXFile() throws ParserConfigurationException, SAXException, IOException;
 
 }
