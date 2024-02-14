62dcbc4909f4205b8cfe504e8a02d6751220f1ff
Marcatelli Francesco <francesco.marcatelli@studio.unibo.it>
2024-02-14 16:59:46 +0100

    Scale the dimension of the MeatBoy and the movement constants

--- src/main/java/it/unibo/model/entity/player/MeatBoyImpl.java
+++ src/main/java/it/unibo/model/entity/player/MeatBoyImpl.java
@@ -16,25 +16,25 @@ public class MeatBoyImpl extends EntityImpl<RectangleHitbox> implements MeatBoy 
      * Represents the default speed value for an entity.
      * This constant is typically used when no specific speed multiplier is set.
      */
-    public static final double SPEED = 3 * Constants.scaleX;
+    public static final double SPEED = 3 * Constants.SCALE_PROPORTION;
 
     /**
      * Represents the maximum jump height for an entity.
      * This constant defines the maximum vertical distance an entity can jump.
      */
-    public static final double MAX_JUMP_HEIGHT = 150 * Constants.scaleY;
+    public static final double MAX_JUMP_HEIGHT = 100 * Constants.SCALE_PROPORTION;
 
     /**
      * Represents the falling speed for an entity.
      * This constant defines the speed at which an entity falls when not supported by a surface.
      */
-    public static final double FALLING_SPEED = 5 * Constants.scaleY;
+    public static final double FALLING_SPEED = 5 * Constants.SCALE_PROPORTION;
 
     /**
      * Represents the jump speed for an entity.
      * This constant defines the initial upward speed when an entity jumps.
      */
-    public static final double JUMP_SPEED = 10 * Constants.scaleY;
+    public static final double JUMP_SPEED = 10 * Constants.SCALE_PROPORTION;
 
     /**
      * Constructs a MeatBoyImpl object with the specified coordinates.
@@ -43,7 +43,7 @@ public class MeatBoyImpl extends EntityImpl<RectangleHitbox> implements MeatBoy 
      * @param y The y-coordinate of the MeatBoy.
      */
     public MeatBoyImpl(final double x, final double y) {
-        super(x, y, new RectangleHitbox(x, y, Constants.TILE_SIZE, Constants.TILE_SIZE)); 
+        super(x, y, new RectangleHitbox(x, y, (int)(Constants.TILE_SIZE * Constants.SCALE_PROPORTION), (int) (Constants.TILE_SIZE * Constants.SCALE_PROPORTION))); 
         this.speedMul = 1;
     }
 
