6f951716512296ec3ab0c422c4d53490b35654c8
Martin Tomassi <martin.tomassi@studio.unibo.it>
2024-02-14 13:33:17 +0100

    Fix Checkstyle Issues of ImageLoader Factory Method

--- src/main/java/it/unibo/view/imagerenderer/factory/loader/ImageLoaderFactoryImpl.java
+++ src/main/java/it/unibo/view/imagerenderer/factory/loader/ImageLoaderFactoryImpl.java
@@ -3,10 +3,15 @@ package it.unibo.view.imagerenderer.factory.loader;
 import it.unibo.view.imagerenderer.loader.ImageLoader;
 import it.unibo.view.imagerenderer.loader.ImageLoaderImpl;
 
+/**
+ * The ImageLoaderFactoryImpl class is an implementation of the ImageLoaderFactory interface.
+ * It provides a concrete implementation of the factory method createImageLoader(), instantiating
+ * ImageLoader objects.
+ */
 public class ImageLoaderFactoryImpl implements ImageLoaderFactory {
 
     @Override
-    public ImageLoader createImageLoader() {
+    public final ImageLoader createImageLoader() {
         return new ImageLoaderImpl();
     }
 
