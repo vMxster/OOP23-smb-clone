334de30212f592fa99ea4d85f2a1924b050430f4
Martin Tomassi <martin.tomassi@studio.unibo.it>
2024-02-14 13:25:38 +0100

    Fix Checkstyle Issues of GameController Factory Method

--- src/main/java/it/unibo/controller/factory/GameControllerFactoryImpl.java
+++ src/main/java/it/unibo/controller/factory/GameControllerFactoryImpl.java
@@ -3,10 +3,15 @@ package it.unibo.controller.factory;
 import it.unibo.controller.GameController;
 import it.unibo.controller.GameControllerImpl;
 
+/**
+ * The GameControllerFactoryImpl class is an implementation of the GameControllerFactory interface.
+ * It provides a concrete implementation of the factory method createGameController(), instantiating
+ * GameController objects.
+ */
 public class GameControllerFactoryImpl implements GameControllerFactory {
 
     @Override
-    public GameController createGameController() {
+    public final GameController createGameController() {
         return new GameControllerImpl();
     }
 
