a1b140c0bb84369090a9cc2446a5497bb73e8895
Martin Tomassi <martin.tomassi@studio.unibo.it>
2024-02-14 13:06:10 +0100

    Added Factory Method Interface of ImageLoader

---
+++ src/main/java/it/unibo/view/imagerenderer/factory/loader/ImageLoaderFactory.java
@@ -0,0 +1,10 @@
+package it.unibo.view.imagerenderer.factory.loader;
+
+import it.unibo.view.imagerenderer.loader.ImageLoader;
+
+public interface ImageLoaderFactory {
+
+    ImageLoader createImageLoader();
+
+}
+
