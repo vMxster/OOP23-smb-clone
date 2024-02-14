9366ae120b826e29138c996daf552e59a3d21c09
Martin Tomassi <martin.tomassi@studio.unibo.it>
2024-02-14 13:31:31 +0100

    Fix Checkstyle Issues of ImageRendererStationary Factory Method

--- src/main/java/it/unibo/view/imagerenderer/factory/gameobjects/ImageRendererStationaryFactoryImpl.java
+++ src/main/java/it/unibo/view/imagerenderer/factory/gameobjects/ImageRendererStationaryFactoryImpl.java
@@ -3,6 +3,11 @@ package it.unibo.view.imagerenderer.factory.gameobjects;
 import it.unibo.view.imagerenderer.gameobjects.ImageRendererStationary;
 import it.unibo.view.imagerenderer.gameobjects.ImageRendererStationaryImpl;
 
+/**
+ * The ImageRendererStationaryFactoryImpl class is an implementation of the ImageRendererStationaryFactory interface.
+ * It provides a concrete implementation of the factory method createImageRendererStationary(), instantiating
+ * ImageRendererStationary objects.
+ */
 public class ImageRendererStationaryFactoryImpl implements ImageRendererStationaryFactory {
 
     @Override
