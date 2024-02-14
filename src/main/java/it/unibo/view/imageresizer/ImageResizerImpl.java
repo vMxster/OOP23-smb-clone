b3e2f4cbf7e751f64fe4e65be9a055b39cba9aa8
Martin Tomassi <martin.tomassi@studio.unibo.it>
2024-02-14 18:53:46 +0100

    Modified ImageResizer Implementation of ImageResizer Interface - Modified resize Method

--- src/main/java/it/unibo/view/imageresizer/ImageResizerImpl.java
+++ src/main/java/it/unibo/view/imageresizer/ImageResizerImpl.java
@@ -5,23 +5,19 @@ import java.awt.image.BufferedImage;
 import java.util.List;
 
 import it.unibo.commons.Constants;
-import it.unibo.controller.GameController;
 
 import java.util.ArrayList;
 
 public class ImageResizerImpl implements ImageResizer {
 
-    private final GameController controller;
-
-    public ImageResizerImpl(final GameController controller) {
-        this.controller = controller;
+    public ImageResizerImpl() {
     }
 
     @Override
     public List<BufferedImage> resize(final List<BufferedImage> images) {
         final List<BufferedImage> newImages = new ArrayList<>();
-        final double scaleX = (double) Constants.SW / images.get(0).getWidth();
-        final double scaleY = (double) Constants.SH / images.get(0).getHeight();
+        final double scaleX = (double) Constants.SW / 800;
+        final double scaleY = (double) Constants.SH / 600;
         for (int i = 0; i<images.size(); i++) {
             int scaledWidth = (int) (images.get(i).getWidth() * scaleX);
             int scaledHeight = (int) (images.get(i).getHeight() * scaleY);
@@ -37,28 +33,6 @@ public class ImageResizerImpl implements ImageResizer {
                 g.dispose();
             }
         }
-
-        for (var elem : controller.getSaws()) {
-            elem.getHitbox().getHitbox().setFrame(
-                elem.getHitbox().getHitbox().getX() * scaleX,
-                elem.getHitbox().getHitbox().getY() * scaleY,
-                (int) (elem.getHitbox().getHitbox().getWidth() * scaleX),
-                (int) (elem.getHitbox().getHitbox().getWidth() * scaleX));
-        }
-
-        for (var elem : controller.getPlatforms()) {
-            elem.getHitbox().getHitbox().setFrame(
-                elem.getHitbox().getHitbox().getX() * scaleX,
-                elem.getHitbox().getHitbox().getY() * scaleY,
-                (int) (elem.getHitbox().getHitbox().getWidth() * scaleX),
-                (int) (elem.getHitbox().getHitbox().getHeight() * scaleY));
-        }
-
-        controller.getBandageGirl().setX(controller.getBandageGirl().getX() * scaleX);
-        controller.getBandageGirl().setY(controller.getBandageGirl().getY() * scaleY);
-        controller.getMeatBoyStartCoord().setX(controller.getMeatBoy().getX() * scaleX);
-        controller.getMeatBoyStartCoord().setY(controller.getMeatBoy().getY() * scaleY);
-
         return newImages;
     }
 
