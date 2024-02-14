f4f8c14a34e3a1518a5d07f3aaedc1b44c994f47
Mattia <mattia.galli9@studio.unibo.it>
2024-02-14 16:12:42 +0100

    Fix Checkstyle Issues of StatisticImpl

--- src/main/java/it/unibo/model/statistic/StatisticImpl.java
+++ src/main/java/it/unibo/model/statistic/StatisticImpl.java
@@ -1,31 +1,52 @@
 package it.unibo.model.statistic;
-
-public class StatisticImpl implements Statistic{
+/**
+ * The Statistic rappresents the implementation methods that 
+ * update the total number of death 
+ * and the time record of the player.
+ */
+public class StatisticImpl implements Statistic {
     private int deaths;
     private int recordTime; 
-    
+
+    /**
+     * Constructs a new Statistic of the player.
+     */
     public StatisticImpl() {
         this.deaths = 0;
         this.recordTime = Integer.MAX_VALUE;
     }
 
+    /**
+     * Add a death every time the player die.
+     */
     @Override
     public void addDeaths() {
         this.deaths++;
     }
-
+    /**
+     * Update the time record every time the player 
+     * makes a better time. 
+     */
     @Override
-    public void updateRecord(int time) {
+    public void updateRecord(final int time) {
         if (time < this.recordTime) {
             this.recordTime = time;
         }
     }
-
+    /**
+     * Returns the total number of deaths.
+     * 
+     * @return total number of deaths.
+     */
     @Override
     public int getDeaths() {
         return this.deaths;
     }
-
+    /**
+     * Returns the best time record.
+     * 
+     * @return the best time record.
+     */
     @Override
     public int getRecordTime() {
         return this.recordTime;
