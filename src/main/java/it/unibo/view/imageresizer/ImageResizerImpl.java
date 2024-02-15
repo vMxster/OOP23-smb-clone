53b47902bdd7c3c651fa299b607d561d852aadce
Martin Tomassi <martin.tomassi@studio.unibo.it>
2024-02-15 12:37:15 +0100

    Modified ImageResizer Implementation of ImageResizer Interface - Modified resize Method using Streams and Lambdas and Added Javadoc

--- src/main/java/it/unibo/view/imageresizer/ImageResizerImpl.java
+++ src/main/java/it/unibo/view/imageresizer/ImageResizerImpl.java
@@ -3,37 +3,41 @@ package it.unibo.view.imageresizer;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import it.unibo.commons.Constants;
 
-import java.util.ArrayList;
-
+/**
+ * Implementation of the ImageResizer interface that resizes a list of BufferedImages.
+ */
 public class ImageResizerImpl implements ImageResizer {
 
-    public ImageResizerImpl() {
-    }
-
+    /**
+     * Resizes a list of BufferedImages.
+     *
+     * @param images The list of BufferedImages to resize.
+     * @return A list of resized BufferedImages.
+     */
     @Override
     public List<BufferedImage> resize(final List<BufferedImage> images) {
-        final List<BufferedImage> newImages = new ArrayList<>();
-        final double scaleX = (double) Constants.SW / 800;
-        final double scaleY = (double) Constants.SH / 600;
-        for (int i = 0; i<images.size(); i++) {
-            int scaledWidth = (int) (images.get(i).getWidth() * scaleX);
-            int scaledHeight = (int) (images.get(i).getHeight() * scaleY);
-            BufferedImage resizedImage = new BufferedImage(scaledWidth, scaledHeight, BufferedImage.TYPE_INT_ARGB);
-            Graphics2D g = resizedImage.createGraphics();
-            if (i==images.size()-1) {
-                g.drawImage(images.get(i), 0,0, (int) (Constants.TILE_SIZE * scaleX) , (int) (Constants.TILE_SIZE * scaleY) , null);
-                newImages.add(resizedImage);
+        return IntStream.range(0, images.size())
+            .mapToObj(i -> {
+                final int scaledWidth = (int) (images.get(i).getWidth() * Constants.SCALE_PROPORTION);
+                final int scaledHeight = (int) (images.get(i).getHeight() * Constants.SCALE_PROPORTION);
+                final BufferedImage resizedImage = new BufferedImage(scaledWidth, scaledHeight, BufferedImage.TYPE_INT_ARGB);
+                final Graphics2D g = resizedImage.createGraphics();
+                g.drawImage(
+                    images.get(i),
+                    0,
+                    0,
+                    (int) (i == images.size() - 1 ? Constants.TILE_SIZE * Constants.SCALE_PROPORTION : scaledWidth),
+                    (int) (i == images.size() - 1 ? Constants.TILE_SIZE * Constants.SCALE_PROPORTION : scaledHeight),
+                    null);
                 g.dispose();
-            } else {
-                g.drawImage(images.get(i), 0, 0, scaledWidth, scaledHeight, null);
-                newImages.add(resizedImage);
-                g.dispose();
-            }
-        }
-        return newImages;
+                return resizedImage;
+            })
+            .collect(Collectors.toList());
     }
 
 }
