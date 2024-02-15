fbec80ab825d07ea7f62326b91a49f034a2a427a
Martin Tomassi <martin.tomassi@studio.unibo.it>
2024-02-15 15:57:07 +0100

    Added Factory Method Implementation of its Interface of TileSet

---
+++ src/main/java/it/unibo/model/tiles/tileset/factory/TileSetFactoryImpl.java
@@ -0,0 +1,17 @@
+package it.unibo.model.tiles.tileset.factory;
+
+import it.unibo.model.tiles.tileset.TileSet;
+import it.unibo.model.tiles.tileset.TileSetImpl;
+
+/**
+ * The TileSetFactoryImpl class is an implementation of the TileSetFactory interface.
+ * It provides functionality to create TileSet objects based on the provided TMX (Tile Map XML).
+ */
+public class TileSetFactoryImpl implements TileSetFactory {
+
+    @Override
+    public final TileSet createTileSet(final String tmx) {
+        return new TileSetImpl(tmx);
+    }
+
+}
