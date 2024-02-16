8bf7d380337e34ca3423bdcff3711fe60f8ddd93
Martin Tomassi <martin.tomassi@studio.unibo.it>
2024-02-16 12:58:47 +0100

    Modified loadObjects Method Implementation using TagType Enum Class

--- src/main/java/it/unibo/model/tiles/loader/gameobjects/TileLoaderGameObjectsImpl.java
+++ src/main/java/it/unibo/model/tiles/loader/gameobjects/TileLoaderGameObjectsImpl.java
@@ -8,6 +8,7 @@ import org.w3c.dom.NodeList;
 
 import it.unibo.commons.Constants;
 import it.unibo.model.documentextractor.DocumentExtractor;
+import it.unibo.model.documentextractor.TagType;
 import it.unibo.model.entity.obstacles.CircularSawImpl;
 import it.unibo.model.entity.obstacles.PlatformImpl;
 import it.unibo.model.tiles.loader.manager.GameObjectType;
@@ -46,29 +47,37 @@ public class TileLoaderGameObjectsImpl implements TileLoaderGameObjects {
     /**
      * Loads objects from the TMX file and populates the TileManager.
      *
-     * @param nameObjects The name of the objects to load ( "rectangle" , "saws" ).
+     * @param nameObjects The name of the objects to load.
      */
     private void loadObjects(final GameObjectType nameObjects) {
-        final NodeList objects = documentExtractor.getElements("objectgroup");
+        final NodeList objects = documentExtractor.getElements(TagType.OBJECTGROUP);
         IntStream.range(0, objects.getLength())
             .mapToObj(i -> (Element) objects.item(i))
             .collect(Collectors.toList()).stream()
-                .filter(node -> nameObjects.toString().equals(((Element) node).getAttribute("name")))
+                .filter(node -> nameObjects.toString()
+                    .equals(((Element) node).getAttribute(TagType.NAME.toString())))
                 .findFirst()
                 .ifPresent(objectGroupElement -> {
-                    final NodeList objectsInGroup = ((Element) objectGroupElement).getElementsByTagName("object");
+                    final NodeList objectsInGroup = ((Element) objectGroupElement)
+                        .getElementsByTagName(TagType.OBJECT.toString());
                     IntStream.range(0, objectsInGroup.getLength())
                         .mapToObj(i -> (Element) objectsInGroup.item(i))
                         .collect(Collectors.toList()).stream()
                             .map(objectNode -> (Element) objectNode)
                             .forEach(objectElement -> {
                                 final int x = Integer.parseInt(
-                                    this.tileLoaderManager.trim(objectElement.getAttribute("x")));
+                                    this.tileLoaderManager.trim(
+                                        objectElement
+                                            .getAttribute(TagType.X.toString())));
                                 final int y = Integer.parseInt(
-                                    this.tileLoaderManager.trim(objectElement.getAttribute("y")));
+                                    this.tileLoaderManager.trim(
+                                        objectElement
+                                            .getAttribute(TagType.Y.toString())));
                                 final int width = Integer.parseInt(
-                                    this.tileLoaderManager.trim(objectElement.getAttribute("width")));
-                                if (GameObjectType.SAWS.toString().equals(nameObjects.toString())) {
+                                    this.tileLoaderManager.trim(
+                                        objectElement
+                                            .getAttribute(TagType.WIDTH.toString())));
+                                if (GameObjectType.SAWS.equals(nameObjects)) {
                                     this.tileLoaderManager.setSaw(new CircularSawImpl(
                                         x * Constants.SCALE_PROPORTION,
                                         y * Constants.SCALE_PROPORTION,
@@ -80,7 +89,8 @@ public class TileLoaderGameObjectsImpl implements TileLoaderGameObjects {
                                         (int) (width * Constants.SCALE_PROPORTION),
                                         (int) (Integer.parseInt(
                                             this.tileLoaderManager.trim(
-                                                objectElement.getAttribute("height"))) * Constants.SCALE_PROPORTION)));
+                                                objectElement
+                                                    .getAttribute(TagType.HEIGHT.toString()))) * Constants.SCALE_PROPORTION)));
                                 }
                             });
                 });
