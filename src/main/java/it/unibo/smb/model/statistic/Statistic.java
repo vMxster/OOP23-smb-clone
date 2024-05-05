ede6f0cf6c63e3c141677aa83572760f6cc6d9e7
Martin Tomassi <martin.tomassi@studio.unibo.it>
2024-05-05 11:14:34 +0200

    Modified Statistic Interface - Added getter on Level Type to which statistics are linked

--- src/main/java/it/unibo/smb/model/statistic/Statistic.java
+++ src/main/java/it/unibo/smb/model/statistic/Statistic.java
@@ -1,6 +1,9 @@
 package it.unibo.smb.model.statistic;
+
+import it.unibo.smb.controller.LevelType;
+
 /**
- * The Statistic rappresents the total number of death 
+ * The Statistic represents the total number of death
  * and the time record of the player.
  */
 public interface Statistic {
@@ -9,22 +12,32 @@ public interface Statistic {
      * Add a death every time the player die.
      */
     void addDeaths();
+
     /**
      * Update the time record every time the player 
      * makes a better time. 
      * @param time the new time made by the player.
      */
     void updateRecord(int time);
+
     /**
      * Returns the total number of deaths.
      * 
      * @return total number of deaths.
      */
     int getDeaths();
+
     /**
      * Returns the best time record.
      * 
      * @return the best time record.
      */
     int getRecordTime();
+
+    /**
+     * Retrieves the level type associated with the current statistic.
+     *
+     * @return The level type of the statistic, indicating the environment and level number.
+     */
+    LevelType getLevelName();
 }
