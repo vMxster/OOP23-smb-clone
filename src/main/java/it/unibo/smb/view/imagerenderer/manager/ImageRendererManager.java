1eb3f6f4dbd79b02981cf605c9a3040c6dab312c
Martin Tomassi <martin.tomassi@studio.unibo.it>
2024-05-05 11:06:56 +0200

    Modified ImageRendererManager Interface - Modified the 'render' method, which now takes the environment of the level as a parameter

--- src/main/java/it/unibo/smb/view/imagerenderer/manager/ImageRendererManager.java
+++ src/main/java/it/unibo/smb/view/imagerenderer/manager/ImageRendererManager.java
@@ -1,18 +1,22 @@
 package it.unibo.smb.view.imagerenderer.manager;
 
+import it.unibo.smb.controller.EnvironmentType;
+
 import java.awt.image.BufferedImage;
 import java.util.List;
 
 /**
- * The ImageRendererManager interface is responsible for managing the rendering of the game map.
+ * The ImageRendererManager interface is responsible for managing the rendering of the game map, including circular saws,
+ * stationary tiles, background, and the character MeatBoy.
  */
 public interface ImageRendererManager {
 
     /**
-     * Manage the rendering of circular Saws, Stationary Tiles, Background and MeatBoy into a list of BufferedImages.
+     * Manages the rendering of circular saws, stationary tiles, background, and MeatBoy into a list of BufferedImages
+     * based on the provided environment type.
      *
+     * @param environmentType the type of environment for which rendering is requested.
      * @return A list of BufferedImages representing the rendered game elements.
      */
-    List<BufferedImage> render();
-
+    List<BufferedImage> render(EnvironmentType environmentType);
 }
