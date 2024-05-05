15d5c980fb5ce60e0cec4dab2ac657bca87669ca
Martin Tomassi <martin.tomassi@studio.unibo.it>
2024-05-05 11:55:29 +0200

    Modified GameWindow Interface and its Swing Implementation - Changed the startup mode of Map loading, update statistics ; Added new Panel types in the selection of them

--- src/main/java/it/unibo/smb/view/window/GameWindow.java
+++ src/main/java/it/unibo/smb/view/window/GameWindow.java
@@ -1,5 +1,8 @@
 package it.unibo.smb.view.window;
 
+import it.unibo.smb.controller.EnvironmentType;
+import it.unibo.smb.controller.GameController;
+
 /**
  * The GameWindow interface represents a window for a game application.
  * It defines methods for initializing window properties and the game panel.
@@ -8,33 +11,46 @@ public interface GameWindow {
 
     /**
      * Enumeration representing different types of panels in a game or application.
-     * Possible values include GAME, MENU, and SCOREBOARD.
+     * Possible values include GAME, MENU, SCOREBOARD, LEVELS, and LOADING.
      */
-    enum PanelType { GAME, MENU, SCOREBOARD }
+    enum PanelType { GAME, MENU, SCOREBOARD, LEVELS, LOADING }
+
+    /**
+     * Starts the game window with the specified environment type and game controller.
+     *
+     * @param env        The environment type of the game.
+     * @param controller The game controller managing the game logic.
+     */
+    void start(EnvironmentType env, GameController controller);
+
+    /**
+     * Updates the statistics displayed in the game window.
+     */
+    void updateStats();
 
     /**
      * Paints the content of the game window.
      * This method is responsible for updating and rendering the content of the game window.
      * It should be called whenever the content needs to be refreshed or repainted.
-     * @param centiSeconds Time in hundredths of a second.
-     * @param currentDeaths number of deaths in the current session.
+     *
+     * @param centiSeconds  Time in hundredths of a second.
+     * @param currentDeaths Number of deaths in the current session.
      */
     void paint(int centiSeconds, int currentDeaths);
 
     /**
-     * This method is responsible for showing a graphical message or UI elements
-     * indicating the player's victory.
+     * Displays a graphical message or UI elements indicating the player's victory.
      */
     void displayVictoryMessage();
 
     /**
-     * This method set visible the current panel.
+     * Sets the current panel visible.
      */
     void setPanelVisible();
 
     /**
-     * This method is responsible for switching between different panels within the game window.
-     * 
+     * Switches between different panels within the game window.
+     *
      * @param type The type of panel to set.
      */
     void switchPanel(PanelType type);
--- src/main/java/it/unibo/smb/view/window/GameWindowSwing.java
+++ src/main/java/it/unibo/smb/view/window/GameWindowSwing.java
@@ -1,6 +1,7 @@
 package it.unibo.smb.view.window;
 
 import java.awt.Font;
+import java.io.Serial;
 import java.util.Objects;
 
 import javax.swing.ImageIcon;
@@ -10,6 +11,7 @@ import javax.swing.JPanel;
 import javax.swing.JLabel;
 
 import it.unibo.smb.commons.Constants;
+import it.unibo.smb.controller.EnvironmentType;
 import it.unibo.smb.controller.GameController;
 import it.unibo.smb.view.imagerenderer.manager.ImageType;
 import it.unibo.smb.view.panel.GameMenu;
@@ -18,6 +20,8 @@ import it.unibo.smb.view.imagerenderer.manager.ImageRendererManager;
 import it.unibo.smb.view.imageresizer.ImageResizer;
 import it.unibo.smb.view.imageresizer.factory.ImageResizerFactoryImpl;
 import it.unibo.smb.view.panel.GamePanel;
+import it.unibo.smb.view.panel.LevelSelectionPanel;
+import it.unibo.smb.view.panel.LoadingPanel;
 import it.unibo.smb.view.panel.Scoreboard;
 
 /**
@@ -26,7 +30,9 @@ import it.unibo.smb.view.panel.Scoreboard;
  */
 public class GameWindowSwing extends JFrame implements GameWindow {
 
-    public static final long serialVersionUID = 3;
+    /** Default serial version UID. */
+    @Serial
+    private static final long serialVersionUID = 4L;
     private static final int INITIAL_TIMER_POSITIONX = (int) (Constants.SW * 0.01);
     private static final int INITIAL_TIMER_POSITIONY = (int) (Constants.SH * 0.01);
     private static final int TIMER_WIDTH = (int) (Constants.SW * 0.08);
@@ -36,13 +42,16 @@ public class GameWindowSwing extends JFrame implements GameWindow {
     private static final int DEATHSFIELD_WIDTH = (int) (Constants.SW * 0.05);
     private static final int DEATHSFIELD_HEIGHT = (int) (Constants.SH * 0.03);
     private static final int FONT_SIZE = 30;
-    private final transient ImageRendererManager renderer;
+
     private final GameMenu menu;
     private final JLabel timerField;
+    private final LoadingPanel loading;
     private final JLabel deathsField;
     private final Scoreboard scoreboard;
     private final transient ImageResizer imageResizer;
-    private final GamePanel gamePanel;
+    private final LevelSelectionPanel levelSelection;
+    private transient ImageRendererManager renderer;
+    private GamePanel gamePanel;
 
     /**
      * Constructs a new instance of GameWindowSwing with the specified GameController.
@@ -53,24 +62,47 @@ public class GameWindowSwing extends JFrame implements GameWindow {
         this.setIconImage(new ImageIcon(Objects.requireNonNull(getClass()
                 .getClassLoader()
                 .getResource(ImageType.MEAT_BOY.toString()))).getImage());
-        this.renderer = new ImageRendererManagerFactoryImpl()
-            .createImageRendererManager(controller);
         this.menu = new GameMenu(controller);
         this.timerField = new JLabel();
         this.deathsField = new JLabel();
-        this.scoreboard = new Scoreboard(controller, this);
+        this.scoreboard = new Scoreboard(controller);
+        this.loading = new LoadingPanel();
         this.imageResizer = new ImageResizerFactoryImpl()
-            .createImageResizer();
-        this.gamePanel = new GamePanel(controller);
-        initializeGamePanel();
+                .createImageResizer();
+        this.levelSelection = new LevelSelectionPanel(controller);
         setContentPane(menu);
         initializeWindowProperties();
+    }
+
+    /**
+     * Starts the game with the specified environment type and controller.
+     *
+     * @param environmentType The environment type.
+     * @param controller The game controller.
+     */
+    @Override
+    public final void start(final EnvironmentType environmentType, final GameController controller) {
+        this.renderer = new ImageRendererManagerFactoryImpl()
+                .createImageRendererManager(controller);
+        this.gamePanel = new GamePanel(controller);
+        initializeGamePanel(environmentType);
+    }
+
+    /**
+     * Updates the statistics displayed in the game window.
+     */
+    @Override
+    public final void updateStats() {
+        this.scoreboard.updateStats();
     }
 
     /**
      * Paints the content of the game window.
      * This method is responsible for updating and rendering the content of the game window.
      * It should be called whenever the content needs to be refreshed or repainted.
+     *
+     * @param centiSeconds  Time in hundredths of a second.
+     * @param currentDeaths Number of deaths in the current session.
      */
     @Override
     public void paint(final int centiSeconds, final int currentDeaths) {
@@ -80,8 +112,7 @@ public class GameWindowSwing extends JFrame implements GameWindow {
     }
 
     /**
-     * This method is responsible for showing a graphical message or UI elements
-     * indicating the player's victory.
+     * Displays a graphical message or UI elements indicating the player's victory.
      */
     @Override
     public void displayVictoryMessage() {
@@ -94,38 +125,38 @@ public class GameWindowSwing extends JFrame implements GameWindow {
     private void initializeWindowProperties() {
         this.setSize(Constants.SW, Constants.SH);
         this.setResizable(false);
-        this.setLocationByPlatform(true);
+        this.setLocationRelativeTo(null);
         this.setTitle("Super Meat Boy Clone");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
     /**
-     * Initializes the game panel.
+     * Initializes the game panel with the specified environment type.
+     *
+     * @param environmentType The environment type for rendering the game panel.
      */
-    private void initializeGamePanel() {
+    private void initializeGamePanel(final EnvironmentType environmentType) {
         this.gamePanel.setLocation(0, 0);
-        this.gamePanel.setImages(this.imageResizer.resize(this.renderer.render()));
+        this.gamePanel.setImages(
+                this.imageResizer.resize(this.renderer.render(environmentType)));
         this.timerField.setFont(new Font("Arial", Font.BOLD, FONT_SIZE));
         this.timerField.setBounds(INITIAL_TIMER_POSITIONX, INITIAL_TIMER_POSITIONY, TIMER_WIDTH, TIMER_HEIGHT);
         this.deathsField.setFont(new Font("Arial", Font.BOLD, FONT_SIZE));
         this.deathsField.setBounds(INITIAL_DEATHSFIELD_POSITIONX,
-            INITIAL_DEATHSFIELD_POSITIONY, DEATHSFIELD_WIDTH, DEATHSFIELD_HEIGHT);
+                INITIAL_DEATHSFIELD_POSITIONY, DEATHSFIELD_WIDTH, DEATHSFIELD_HEIGHT);
         this.gamePanel.add(timerField);
         this.gamePanel.add(deathsField);
     }
 
-    /**
-     * Set the game panel visible.
-     */
     @Override
-    public void setPanelVisible() {
+    public final void setPanelVisible() {
         this.setVisible(true);
     }
 
     /**
-     * Switches the content pane to the game panel.
-     * 
-     * @param type Type of panel to set.
+     * Switches the content pane to the specified panel type.
+     *
+     * @param type The type of panel to set.
      */
     @Override
     public void switchPanel(final PanelType type) {
@@ -146,7 +177,8 @@ public class GameWindowSwing extends JFrame implements GameWindow {
             case GAME -> gamePanel;
             case MENU -> menu;
             case SCOREBOARD -> scoreboard;
-            default -> throw new IllegalArgumentException();
+            case LEVELS -> levelSelection;
+            case LOADING -> loading;
         };
     }
 }
